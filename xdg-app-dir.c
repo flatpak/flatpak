7c8fb83f2047576e7c2468727e4f0a0bad8e6820
Alexander Larsson <alexl@redhat.com>
2015-02-05 18:15:35 +0100

    Create files/.ref when deploying
    This will be used later to avoid deleting active mounts.
    They have to be created during deploy, because we are
    then guaranteed to be able to write to the directory,
    and we don't want the resulting file to be hardlinked
    to any other deployment (as then locks could be shared).

--- xdg-app-dir.c
+++ xdg-app-dir.c
@@ -752,6 +752,7 @@ xdg_app_dir_deploy (XdgAppDir *self,
   gs_unref_object GFileInfo *file_info = NULL;
   gs_unref_object GFile *deploy_base = NULL;
   gs_unref_object GFile *checkoutdir = NULL;
+  gs_unref_object GFile *dotref = NULL;
   gs_unref_object GFile *export = NULL;
   gs_unref_object GFile *exports = NULL;
 
@@ -830,6 +831,11 @@ xdg_app_dir_deploy (XdgAppDir *self,
                                   checkoutdir,
                                   OSTREE_REPO_FILE (root), file_info,
                                   cancellable, error))
+    goto out;
+
+  dotref = g_file_resolve_relative_path (checkoutdir, "files/.ref");
+  if (!g_file_replace_contents (dotref, "", 0, NULL, FALSE,
+                                G_FILE_CREATE_NONE, NULL, cancellable, error))
     goto out;
 
   is_app = g_str_has_prefix (ref, "app");
