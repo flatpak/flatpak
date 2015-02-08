4ff10fee86b40ac30ca1f412e4d6ac8b4d20d70e
Alexander Larsson <alexl@redhat.com>
2015-02-08 23:37:47 +0100

    Add support for --with-priv-mode=caps
    This allows you to set the privs for the helper via file
    capabilities instead of setuid.
    You can also set the mode to none, but then you have to
    manually set either setuid or filecaps (for instance via
    a packaging script).

--- xdg-app-helper.c
+++ xdg-app-helper.c
@@ -1074,13 +1074,16 @@ acquire_caps (void)
   struct __user_cap_header_struct hdr;
   struct __user_cap_data_struct data;
 
-  /* Tell kernel not clear capabilities when dropping root */
-  if (prctl (PR_SET_KEEPCAPS, 1, 0, 0, 0) < 0)
-    die_with_error ("prctl(PR_SET_KEEPCAPS) failed");
+  if (getuid () != geteuid ())
+    {
+      /* Tell kernel not clear capabilities when dropping root */
+      if (prctl (PR_SET_KEEPCAPS, 1, 0, 0, 0) < 0)
+	die_with_error ("prctl(PR_SET_KEEPCAPS) failed");
 
-  /* Drop root uid, but retain the required permitted caps */
-  if (setuid (getuid ()) < 0)
-    die_with_error ("unable to drop privs");
+      /* Drop root uid, but retain the required permitted caps */
+      if (setuid (getuid ()) < 0)
+	die_with_error ("unable to drop privs");
+    }
 
   memset (&hdr, 0, sizeof(hdr));
   hdr.version = _LINUX_CAPABILITY_VERSION;
