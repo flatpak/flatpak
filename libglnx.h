5ac3f4a119eefa8d240bc5b35c6e447497f59d11
Colin Walters <walters@verbum.org>
2015-02-14 11:19:08 -0500

    Initial commit; backport of GLib autoptr code
    This module is a new successor to libgsystem.  It currently contains a
    backport of the GLib cleanup macros, but soon more Linux-specific code
    will be added.

---
+++ libglnx.h
@@ -0,0 +1,31 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright (C) 2012,2013,2015 Colin Walters <walters@verbum.org>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#pragma once
+
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+#include <glnx-local-alloc.h>
+#include <glnx-backport-autoptr.h>
+#include <glnx-backport-autocleanups.h>
+
+G_END_DECLS
