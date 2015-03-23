07cd13e757359e955870155135366a290d95bb3e
Alexander Larsson <alexl@redhat.com>
2015-03-23 12:17:58 +0100

    Add copyright headers

--- xdg-app-helper.c
+++ xdg-app-helper.c
@@ -1,7 +1,7 @@
 /* xdg-app-helper
  * Copyright (C) 2014 Alexander Larsson
  *
- * This probram is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
--- xdg-app-run.h
+++ xdg-app-run.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright © 2014 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *       Alexander Larsson <alexl@redhat.com>
+ */
+
 #ifndef __XDG_APP_RUN_H__
 #define __XDG_APP_RUN_H__
 
