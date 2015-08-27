f6657901c4c7b7d1b0ed11f24dac3a521a60889b
Alexander Larsson <alexl@redhat.com>
2015-08-27 19:42:15 +0200

    utils: Add AUTOLOCK macro

--- lib/xdg-app-utils.h
+++ lib/xdg-app-utils.h
@@ -129,6 +129,22 @@ g_strv_subset (const gchar * const *strv,
   return NULL;
 }
 
+static inline void xdg_app_auto_unlock_helper (GMutex **mutex)
+{
+  if (*mutex)
+    g_mutex_unlock (*mutex);
+}
+
+static inline GMutex *xdg_app_auto_lock_helper (GMutex *mutex)
+{
+  if (mutex)
+    g_mutex_lock (mutex);
+  return mutex;
+}
+
+
+#define AUTOLOCK(name) __attribute__((cleanup(xdg_app_auto_unlock_helper))) GMutex * G_PASTE(auto_unlock, __LINE__) = xdg_app_auto_lock_helper (&G_LOCK_NAME (name))
+
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(OstreeRepo, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(OstreeMutableTree, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(OstreeAsyncProgress, g_object_unref)
