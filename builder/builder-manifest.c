f710eb93223b32340550a1365f31e544196def45
Alexander Larsson <alexl@redhat.com>
2015-11-26 14:50:21 +0100

    Correct license, we're LGPL 2+, not 3+
    Some files accidentally got the LGPL 3+ header, but we
    want to be LGPL2+.

--- builder/builder-manifest.c
+++ builder/builder-manifest.c
@@ -4,7 +4,7 @@
  *
  * This file is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of the
+ * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
  *
  * This file is distributed in the hope that it will be useful, but
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors:
