6558c812af2df0fbf630fac45c30d28edaff2d55
Alexander Larsson <alexl@redhat.com>
2015-12-07 16:16:43 +0100

    lib: Add some more debug spew to test-lib

--- lib/test-lib.c
+++ lib/test-lib.c
@@ -14,6 +14,7 @@ main (int argc, char *argv[])
 
   installation = xdg_app_installation_new_user ();
 
+  g_print ("**** Listing all installed apps\n");
   apps = xdg_app_installation_list_installed_refs (installation,
                                                    XDG_APP_REF_KIND_APP,
                                                    NULL, NULL);
@@ -32,6 +33,7 @@ main (int argc, char *argv[])
       g_print ("metadata:\n%s\n", xdg_app_installed_ref_load_metadata (apps[i], NULL, NULL));
     }
 
+  g_print ("\n**** Listing all installed runtimes\n");
   runtimes = xdg_app_installation_list_installed_refs (installation,
                                                        XDG_APP_REF_KIND_RUNTIME,
                                                        NULL, NULL);
@@ -49,6 +51,7 @@ main (int argc, char *argv[])
                xdg_app_installed_ref_get_current (runtimes[i]));
     }
 
+  g_print ("\n**** Getting installed gedit master\n");
   app1 = xdg_app_installation_get_installed_ref (installation, 
                                                  XDG_APP_REF_KIND_APP,
                                                  "org.gnome.gedit",
@@ -67,6 +70,7 @@ main (int argc, char *argv[])
       xdg_app_installed_ref_launch (app1, NULL, NULL);
     }
 
+  g_print ("\n**** Getting current installed gedit\n");
   app2 = xdg_app_installation_get_current_installed_app (installation, 
                                                          "org.gnome.gedit",
                                                          NULL, NULL);
@@ -82,12 +86,14 @@ main (int argc, char *argv[])
              xdg_app_installed_ref_get_current (app2));
 
 
+  g_print ("\n**** Listing remotes\n");
   remotes = xdg_app_installation_list_remotes (installation,
                                                NULL, NULL);
 
   for (i = 0; remotes[i] != NULL; i++)
     {
-      g_print ("%s %s %s %d %d\n",
+      GError *error = NULL;
+      g_print ("\nRemote: %s %s %s %d %d\n",
                xdg_app_remote_get_name (remotes[i]),
                xdg_app_remote_get_url (remotes[i]),
                xdg_app_remote_get_title (remotes[i]),
