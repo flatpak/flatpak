eba6a1e552ec9eb14caf74b64a9d2d6f51873fcb
Alexander Larsson <alexl@redhat.com>
2016-02-25 19:03:09 +0100

    Add appdata to test apps

--- tests/make-test-app.sh
+++ tests/make-test-app.sh
@@ -14,6 +14,25 @@ echo "Hello world, from a sandbox"
 EOF
 chmod a+x ${DIR}/files/bin/hello.sh
 
+mkdir -p ${DIR}/files/share/app-info/xmls
+mkdir -p ${DIR}/files/share/app-info/icons/xdg-app/64x64
+gzip -c > ${DIR}/files/share/app-info/xmls/org.test.Hello.xml.gz <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<components version="0.8">
+  <component type="desktop">
+    <id>org.test.Hello.desktop</id>
+    <name>Hello world test app</name>
+    <summary>Print a greeting</summary>
+    <description><p>This is a test app.</p></description>
+    <categories>
+      <category>Utility</category>
+    </categories>
+    <icon height="64" width="64" type="cached">64x64/org.gnome.gedit.png</icon>
+  </component>
+</components>
+EOF
+cp org.test.Hello.png ${DIR}/files/share/app-info/icons/xdg-app/64x64/
+
 xdg-app build-finish --command=hello.sh ${DIR}
 xdg-app build-export repo ${DIR}
 rm -rf ${DIR}
