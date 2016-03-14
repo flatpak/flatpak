56872d1602a53af0147f75020f1286814980c09d
Alexander Larsson <alexl@redhat.com>
2016-03-14 14:10:19 +0100

    Never use gvfs in the session helper

--- session-helper/xdg-app-session-helper.c
+++ session-helper/xdg-app-session-helper.c
@@ -132,6 +132,8 @@ main (int    argc,
 
   setlocale (LC_ALL, "");
 
+  g_setenv ("GIO_USE_VFS", "local", TRUE);
+
   g_set_prgname (argv[0]);
 
   monitor_dir = g_build_filename (g_get_user_runtime_dir (), "xdg-app-monitor", NULL);
