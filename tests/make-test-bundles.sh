9957cc3a14688c34952436c14c040766a3ccecc8
Alexander Larsson <alexl@redhat.com>
2016-05-10 11:10:51 +0200

    tests: Name bundles .flatpak

--- tests/make-test-bundles.sh
+++ tests/make-test-bundles.sh
@@ -9,18 +9,18 @@ URL=file://`pwd`/repo
 
 REF=`(cd repo/refs/heads; echo app/org.test.Hello/*/master)`
 
-flatpak build-bundle repo hello.pak org.test.Hello
-flatpak build-bundle repo hello-key.pak --gpg-keys=test-keyring/pubring.gpg org.test.Hello
-flatpak build-bundle repo --repo-url=${URL} hello-origin.pak org.test.Hello
-flatpak build-bundle repo --repo-url=${URL} --gpg-keys=test-keyring/pubring.gpg hello-key-origin.pak org.test.Hello
+flatpak build-bundle repo hello.flatpak org.test.Hello
+flatpak build-bundle repo hello-key.flatpak --gpg-keys=test-keyring/pubring.gpg org.test.Hello
+flatpak build-bundle repo --repo-url=${URL} hello-origin.flatpak org.test.Hello
+flatpak build-bundle repo --repo-url=${URL} --gpg-keys=test-keyring/pubring.gpg hello-key-origin.flatpak org.test.Hello
 
 ostree gpg-sign --repo=repo --gpg-homedir=test-keyring ${REF} 7B0961FD
 
-flatpak build-bundle repo hello-signed.pak org.test.Hello
-flatpak build-bundle repo hello-key-signed.pak --gpg-keys=test-keyring/pubring.gpg org.test.Hello
-flatpak build-bundle repo --repo-url=${URL} hello-origin-signed.pak org.test.Hello
-flatpak build-bundle repo --repo-url=${URL} --gpg-keys=test-keyring/pubring.gpg hello-key-origin-signed.pak org.test.Hello
+flatpak build-bundle repo hello-signed.flatpak org.test.Hello
+flatpak build-bundle repo hello-key-signed.flatpak --gpg-keys=test-keyring/pubring.gpg org.test.Hello
+flatpak build-bundle repo --repo-url=${URL} hello-origin-signed.flatpak org.test.Hello
+flatpak build-bundle repo --repo-url=${URL} --gpg-keys=test-keyring/pubring.gpg hello-key-origin-signed.flatpak org.test.Hello
 
 REF=`(cd repo/refs/heads; echo runtime/org.test.Platform/*/master)`
 ostree gpg-sign --repo=repo --gpg-homedir=test-keyring ${REF} 7B0961FD
-flatpak build-bundle --runtime repo --repo-url=${URL} --gpg-keys=test-keyring/pubring.gpg platform.pak org.test.Platform
+flatpak build-bundle --runtime repo --repo-url=${URL} --gpg-keys=test-keyring/pubring.gpg platform.flatpak org.test.Platform
