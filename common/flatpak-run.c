5e95d81d455737a7e71f2ce5192d647884611d12
Alexander Larsson <alexl@redhat.com>
2016-06-09 16:03:54 +0200

    Allow access to all portals

--- common/flatpak-run.c
+++ common/flatpak-run.c
@@ -2307,7 +2307,7 @@ static void
 add_default_permissions (FlatpakContext *app_context)
 {
   flatpak_context_set_session_bus_policy (app_context,
-                                          "org.freedesktop.portal.Documents",
+                                          "org.freedesktop.portal.*",
                                           FLATPAK_POLICY_TALK);
 }
 
