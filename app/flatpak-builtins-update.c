224c4ee9e65dd39931cc1eb839040147b0a7da0d
Alexander Larsson <alexl@redhat.com>
2016-10-25 14:53:11 +0200

    update --appstream: Don't stop at first error

--- app/flatpak-builtins-update.c
+++ app/flatpak-builtins-update.c
@@ -81,13 +81,15 @@ update_appstream (FlatpakDir *dir, const char *remote, GCancellable *cancellable
 
       for (i = 0; remotes[i] != NULL; i++)
         {
+          g_autoptr(GError) local_error = NULL;
+
           if (flatpak_dir_get_remote_disabled (dir, remotes[i]))
             continue;
 
           g_print (_("Updating appstream for remote %s\n"), remotes[i]);
           if (!flatpak_dir_update_appstream (dir, remotes[i], opt_arch, &changed,
-                                             NULL, cancellable, error))
-            return FALSE;
+                                             NULL, cancellable, &local_error))
+            g_printerr ("Error updating: %s\n", local_error->message);
         }
     }
   else
