flatpak/flatpak

commit eeaa83209517852baea746227489daaaefc5eb1b

Replace bash completion shell script with C-based version
This doesn't do everything yet, but its got the basics covered.

app/flatpak-builtins-info.c
@@ -165,3 +165,102 @@ flatpak_builtin_info (int argc, char **argv, GCancellable *cancellable, GError *
 
   return TRUE;
 }
+
+gboolean
+flatpak_complete_info (FlatpakCompletion *completion)
+{
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(FlatpakDir) user_dir = NULL;
+  g_autoptr(FlatpakDir) system_dir = NULL;
+  g_autoptr(GError) error = NULL;
+  g_auto(GStrv) refs = NULL;
+  int i;
+
+  context = g_option_context_new ("");
+  if (!flatpak_option_context_parse (context, options, &completion->argc, &completion->argv, FLATPAK_BUILTIN_FLAG_NO_DIR, NULL, NULL, NULL))
+    return FALSE;
+
+  if (!opt_app && !opt_runtime)
+    opt_app = opt_runtime = TRUE;
+
+  if (!opt_user && !opt_system)
+    opt_user = opt_system = TRUE;
+
+  if (opt_user)
+    user_dir = flatpak_dir_get_user ();
+
+  if (opt_system)
+    system_dir = flatpak_dir_get_system ();
+
+  switch (completion->argc)
+    {
+    case 0:
+    case 1: /* NAME */
+      flatpak_complete_options (completion, global_entries);
+      flatpak_complete_options (completion, options);
+
+      if (user_dir)
+        {
+          g_auto(GStrv) refs = flatpak_dir_find_installed_refs (user_dir, NULL, NULL, opt_arch,
+                                                                opt_app, opt_runtime, &error);
+          if (refs == NULL)
+            flatpak_completion_debug ("find local refs error: %s", error->message);
+          for (i = 0; refs != NULL && refs[i] != NULL; i++)
+            {
+              g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+              if (parts)
+                flatpak_complete_word (completion, "%s ", parts[1]);
+            }
+        }
+
+      if (system_dir)
+        {
+          g_auto(GStrv) refs = flatpak_dir_find_installed_refs (system_dir, NULL, NULL, opt_arch,
+                                                                opt_app, opt_runtime, &error);
+          if (refs == NULL)
+            flatpak_completion_debug ("find local refs error: %s", error->message);
+          for (i = 0; refs != NULL && refs[i] != NULL; i++)
+            {
+              g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+              if (parts)
+                flatpak_complete_word (completion, "%s ", parts[1]);
+            }
+        }
+      break;
+
+    case 2: /* BRANCH */
+      if (user_dir)
+        {
+          g_auto(GStrv) refs = flatpak_dir_find_installed_refs (user_dir, completion->argv[1], NULL, opt_arch,
+                                                                opt_app, opt_runtime, &error);
+          if (refs == NULL)
+            flatpak_completion_debug ("find remote refs error: %s", error->message);
+          for (i = 0; refs != NULL && refs[i] != NULL; i++)
+            {
+              g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+              if (parts)
+                flatpak_complete_word (completion, "%s ", parts[3]);
+            }
+        }
+      if (user_dir)
+        {
+          g_auto(GStrv) refs = flatpak_dir_find_installed_refs (user_dir, completion->argv[1], NULL, opt_arch,
+                                                                opt_app, opt_runtime, &error);
+          if (refs == NULL)
+            flatpak_completion_debug ("find remote refs error: %s", error->message);
+          for (i = 0; refs != NULL && refs[i] != NULL; i++)
+            {
+              g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+              if (parts)
+                flatpak_complete_word (completion, "%s ", parts[3]);
+            }
+        }
+
+      break;
+
+    default:
+      break;
+    }
+
+  return TRUE;
+}

app/flatpak-builtins-install.c
@@ -52,7 +52,7 @@ static GOptionEntry options[] = {
   { "app", 0, 0, G_OPTION_ARG_NONE, &opt_app, "Look for app with the specified name", },
   { "bundle", 0, 0, G_OPTION_ARG_NONE, &opt_bundle, "Install from local bundle file", },
   { "gpg-file", 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &opt_gpg_file, "Check bundle signatures with GPG key from FILE (- for stdin)", "FILE" },
-  { "subpath", 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &opt_subpaths, "Only install this subpath", "path" },
+  { "subpath", 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &opt_subpaths, "Only install this subpath", "PATH" },
   { NULL }
 };
 
@@ -249,6 +249,79 @@ flatpak_builtin_install (int argc, char **argv, GCancellable *cancellable, GErro
                             NULL,
                             cancellable, error))
     return FALSE;
+
+  return TRUE;
+}
+
+gboolean
+flatpak_complete_install (FlatpakCompletion *completion)
+{
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(FlatpakDir) dir = NULL;
+  g_autoptr(GError) error = NULL;
+  g_auto(GStrv) refs = NULL;
+  int i;
+
+  context = g_option_context_new ("");
+  if (!flatpak_option_context_parse (context, options, &completion->argc, &completion->argv, 0, &dir, NULL, NULL))
+    return FALSE;
+
+  if (!opt_app && !opt_runtime)
+    opt_app = opt_runtime = TRUE;
+
+  flatpak_completion_debug ("install argc %d", completion->argc);
+
+  switch (completion->argc)
+    {
+    case 0:
+    case 1: /* REMOTE */
+      flatpak_complete_options (completion, global_entries);
+      flatpak_complete_options (completion, options);
+      flatpak_complete_options (completion, user_entries);
+
+      {
+        g_auto(GStrv) remotes = flatpak_dir_list_remotes (dir, NULL, NULL);
+        if (remotes == NULL)
+          return FALSE;
+        for (i = 0; remotes[i] != NULL; i++)
+          flatpak_complete_word (completion, "%s ", remotes[i]);
+      }
+
+      break;
+
+    case 2: /* Name */
+      refs = flatpak_dir_find_remote_refs (dir, completion->argv[1], NULL, NULL,
+                                           opt_arch, opt_app, opt_runtime,
+                                           NULL, &error);
+      if (refs == NULL)
+        flatpak_completion_debug ("find remote refs error: %s", error->message);
+      for (i = 0; refs != NULL && refs[i] != NULL; i++)
+        {
+          g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+          if (parts)
+            flatpak_complete_word (completion, "%s ", parts[1]);
+        }
+
+      break;
+
+    case 3: /* Branch */
+      refs = flatpak_dir_find_remote_refs (dir, completion->argv[1], completion->argv[2], NULL,
+                                           opt_arch, opt_app, opt_runtime,
+                                           NULL, &error);
+      if (refs == NULL)
+        flatpak_completion_debug ("find remote refs error: %s", error->message);
+      for (i = 0; refs != NULL && refs[i] != NULL; i++)
+        {
+          g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+          if (parts)
+            flatpak_complete_word (completion, "%s ", parts[3]);
+        }
+
+      break;
+
+    default:
+      break;
+    }
 
   return TRUE;
 }

app/flatpak-builtins-list.c
@@ -262,3 +262,11 @@ flatpak_builtin_list (int argc, char **argv, GCancellable *cancellable, GError *
 
   return TRUE;
 }
+
+gboolean
+flatpak_complete_list (FlatpakCompletion *completion)
+{
+  flatpak_complete_options (completion, global_entries);
+  flatpak_complete_options (completion, options);
+  return TRUE;
+}

app/flatpak-builtins-run.c
@@ -135,3 +135,65 @@ flatpak_builtin_run (int argc, char **argv, GCancellable *cancellable, GError **
   /* Not actually reached... */
   return TRUE;
 }
+
+gboolean
+flatpak_complete_run (FlatpakCompletion *completion)
+{
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(FlatpakDir) user_dir = NULL;
+  g_autoptr(FlatpakDir) system_dir = NULL;
+  g_autoptr(GError) error = NULL;
+  int i;
+  g_autoptr(FlatpakContext) arg_context = NULL;
+
+  context = g_option_context_new ("");
+
+  arg_context = flatpak_context_new ();
+  g_option_context_add_group (context, flatpak_context_get_options (arg_context));
+
+  if (!flatpak_option_context_parse (context, options, &completion->argc, &completion->argv,
+                                     FLATPAK_BUILTIN_FLAG_NO_DIR, NULL, NULL, NULL))
+    return FALSE;
+
+  switch (completion->argc)
+    {
+    case 0:
+    case 1: /* NAME */
+      flatpak_complete_options (completion, global_entries);
+      flatpak_complete_options (completion, options);
+      flatpak_context_complete (arg_context, completion);
+
+      user_dir = flatpak_dir_get_user ();
+      {
+        g_auto(GStrv) refs = flatpak_dir_find_installed_refs (user_dir, NULL, NULL, opt_arch,
+                                                              TRUE, FALSE, &error);
+        if (refs == NULL)
+          flatpak_completion_debug ("find local refs error: %s", error->message);
+        for (i = 0; refs != NULL && refs[i] != NULL; i++)
+          {
+            g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+            if (parts)
+              flatpak_complete_word (completion, "%s ", parts[1]);
+          }
+      }
+
+      system_dir = flatpak_dir_get_system ();
+      {
+        g_auto(GStrv) refs = flatpak_dir_find_installed_refs (system_dir, NULL, NULL, opt_arch,
+                                                              TRUE, FALSE, &error);
+        if (refs == NULL)
+          flatpak_completion_debug ("find local refs error: %s", error->message);
+        for (i = 0; refs != NULL && refs[i] != NULL; i++)
+          {
+            g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+            if (parts)
+              flatpak_complete_word (completion, "%s ", parts[1]);
+          }
+      }
+
+      break;
+
+    }
+
+  return TRUE;
+}

app/flatpak-builtins-uninstall.c
@@ -57,7 +57,7 @@ flatpak_builtin_uninstall (int argc, char **argv, GCancellable *cancellable, GEr
   gboolean is_app;
   FlatpakHelperUninstallFlags flags = 0;
 
-  context = g_option_context_new ("APP [BRANCH] - Uninstall an application");
+  context = g_option_context_new ("NAME [BRANCH] - Uninstall an application");
 
   if (!flatpak_option_context_parse (context, options, &argc, &argv, 0, &dir, cancellable, error))
     return FALSE;
@@ -91,6 +91,62 @@ flatpak_builtin_uninstall (int argc, char **argv, GCancellable *cancellable, GEr
   if (!flatpak_dir_uninstall (dir, ref, flags,
                               cancellable, error))
     return FALSE;
+
+  return TRUE;
+}
+
+gboolean
+flatpak_complete_uninstall (FlatpakCompletion *completion)
+{
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(FlatpakDir) dir = NULL;
+  g_autoptr(GError) error = NULL;
+  g_auto(GStrv) refs = NULL;
+  int i;
+
+  context = g_option_context_new ("");
+  if (!flatpak_option_context_parse (context, options, &completion->argc, &completion->argv, 0, &dir, NULL, NULL))
+    return FALSE;
+
+  if (!opt_app && !opt_runtime)
+    opt_app = opt_runtime = TRUE;
+
+  switch (completion->argc)
+    {
+    case 0:
+    case 1: /* NAME */
+      flatpak_complete_options (completion, global_entries);
+      flatpak_complete_options (completion, options);
+      flatpak_complete_options (completion, user_entries);
+
+      refs = flatpak_dir_find_installed_refs (dir, NULL, NULL, opt_arch,
+                                              opt_app, opt_runtime, &error);
+      if (refs == NULL)
+        flatpak_completion_debug ("find local refs error: %s", error->message);
+      for (i = 0; refs != NULL && refs[i] != NULL; i++)
+        {
+          g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+          if (parts)
+            flatpak_complete_word (completion, "%s ", parts[1]);
+        }
+      break;
+
+    case 2: /* Branch */
+      refs = flatpak_dir_find_installed_refs (dir, completion->argv[1], NULL, opt_arch,
+                                              opt_app, opt_runtime, &error);
+      if (refs == NULL)
+        flatpak_completion_debug ("find remote refs error: %s", error->message);
+      for (i = 0; refs != NULL && refs[i] != NULL; i++)
+        {
+          g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+          if (parts)
+            flatpak_complete_word (completion, "%s ", parts[3]);
+        }
+      break;
+
+    default:
+      break;
+    }
 
   return TRUE;
 }

app/flatpak-builtins-update.c
@@ -50,7 +50,7 @@ static GOptionEntry options[] = {
   { "runtime", 0, 0, G_OPTION_ARG_NONE, &opt_runtime, "Look for runtime with the specified name", },
   { "app", 0, 0, G_OPTION_ARG_NONE, &opt_app, "Look for app with the specified name", },
   { "appstream", 0, 0, G_OPTION_ARG_NONE, &opt_appstream, "Update appstream for remote", },
-  { "subpath", 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &opt_subpaths, "Only update this subpath", "path" },
+  { "subpath", 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &opt_subpaths, "Only update this subpath", "PATH" },
   { NULL }
 };
 
@@ -107,7 +107,6 @@ flatpak_builtin_update (int           argc,
   g_autoptr(FlatpakDir) dir = NULL;
   const char *name = NULL;
   const char *branch = NULL;
-  const char *arch = NULL;
   gboolean failed = FALSE;
   int i;
 
@@ -125,9 +124,7 @@ flatpak_builtin_update (int           argc,
     branch = argv[2];
 
   if (opt_arch == NULL)
-    arch = flatpak_get_arch ();
-  else
-    arch = opt_arch;
+    opt_arch = (char *)flatpak_get_arch ();
 
   if (!opt_app && !opt_runtime)
     opt_app = opt_runtime = TRUE;
@@ -155,7 +152,7 @@ flatpak_builtin_update (int           argc,
               if (name != NULL && strcmp (parts[1], name) != 0)
                 continue;
 
-              if (strcmp (parts[2], arch) != 0)
+              if (strcmp (parts[2], opt_arch) != 0)
                 continue;
 
               g_print ("Updating application %s %s\n", parts[1], parts[3]);
@@ -186,7 +183,7 @@ flatpak_builtin_update (int           argc,
               if (name != NULL && strcmp (parts[1], name) != 0)
                 continue;
 
-              if (strcmp (parts[2], arch) != 0)
+              if (strcmp (parts[2], opt_arch) != 0)
                 continue;
 
               g_print ("Updating runtime %s %s\n", parts[1], parts[3]);
@@ -207,7 +204,7 @@ flatpak_builtin_update (int           argc,
       ref = flatpak_dir_find_installed_ref (dir,
                                             name,
                                             branch,
-                                            arch,
+                                            opt_arch,
                                             opt_app, opt_runtime, &is_app,
                                             error);
       if (ref == NULL)
@@ -223,6 +220,62 @@ flatpak_builtin_update (int           argc,
 
   if (failed)
     return flatpak_fail (error, "One or more updates failed");
+
+  return TRUE;
+}
+
+gboolean
+flatpak_complete_update (FlatpakCompletion *completion)
+{
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(FlatpakDir) dir = NULL;
+  g_autoptr(GError) error = NULL;
+  g_auto(GStrv) refs = NULL;
+  int i;
+
+  context = g_option_context_new ("");
+  if (!flatpak_option_context_parse (context, options, &completion->argc, &completion->argv, 0, &dir, NULL, NULL))
+    return FALSE;
+
+  if (!opt_app && !opt_runtime)
+    opt_app = opt_runtime = TRUE;
+
+  switch (completion->argc)
+    {
+    case 0:
+    case 1: /* NAME */
+      flatpak_complete_options (completion, global_entries);
+      flatpak_complete_options (completion, options);
+      flatpak_complete_options (completion, user_entries);
+
+      refs = flatpak_dir_find_installed_refs (dir, NULL, NULL, opt_arch,
+                                              opt_app, opt_runtime, &error);
+      if (refs == NULL)
+        flatpak_completion_debug ("find local refs error: %s", error->message);
+      for (i = 0; refs != NULL && refs[i] != NULL; i++)
+        {
+          g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+          if (parts)
+            flatpak_complete_word (completion, "%s \n", parts[1]);
+        }
+      break;
+
+    case 2: /* Branch */
+      refs = flatpak_dir_find_installed_refs (dir, completion->argv[1], NULL, opt_arch,
+                                              opt_app, opt_runtime, &error);
+      if (refs == NULL)
+        flatpak_completion_debug ("find remote refs error: %s", error->message);
+      for (i = 0; refs != NULL && refs[i] != NULL; i++)
+        {
+          g_auto(GStrv) parts = flatpak_decompose_ref (refs[i], NULL);
+          if (parts)
+            flatpak_complete_word (completion, "%s ", parts[3]);
+        }
+      break;
+
+    default:
+      break;
+    }
 
   return TRUE;
 }

app/flatpak-builtins.h
@@ -24,6 +24,7 @@
 #include <ostree.h>
 #include <gio/gio.h>
 
+#include "flatpak-utils.h"
 #include "flatpak-dir.h"
 
 G_BEGIN_DECLS
@@ -42,36 +43,42 @@ gboolean flatpak_option_context_parse (GOptionContext     *context,
                                        GCancellable       *cancellable,
                                        GError            **error);
 
+extern GOptionEntry user_entries[];
+extern GOptionEntry global_entries[];
+
 gboolean usage_error (GOptionContext *context,
                       const char     *message,
                       GError        **error);
 
-#define BUILTINPROTO(name) gboolean flatpak_builtin_ ## name (int argc, char **argv, GCancellable * cancellable, GError * *error)
+#define BUILTINPROTO(name) \
+  gboolean flatpak_builtin_ ## name (int argc, char **argv, GCancellable * cancellable, GError * *error); \
+  gboolean flatpak_complete_ ## name (FlatpakCompletion *completion);
 
-BUILTINPROTO (add_remote);
-BUILTINPROTO (modify_remote);
-BUILTINPROTO (delete_remote);
-BUILTINPROTO (ls_remote);
-BUILTINPROTO (list_remotes);
-BUILTINPROTO (install);
-BUILTINPROTO (update);
-BUILTINPROTO (make_current_app);
-BUILTINPROTO (uninstall);
-BUILTINPROTO (install_bundle);
-BUILTINPROTO (list);
-BUILTINPROTO (info);
-BUILTINPROTO (run);
-BUILTINPROTO (enter);
-BUILTINPROTO (build_init);
-BUILTINPROTO (build);
-BUILTINPROTO (build_finish);
-BUILTINPROTO (build_sign);
-BUILTINPROTO (build_export);
-BUILTINPROTO (build_bundle);
-BUILTINPROTO (build_import);
-BUILTINPROTO (build_update_repo);
-BUILTINPROTO (export_file);
-BUILTINPROTO (override);
+
+BUILTINPROTO (add_remote)
+BUILTINPROTO (modify_remote)
+BUILTINPROTO (delete_remote)
+BUILTINPROTO (ls_remote)
+BUILTINPROTO (list_remotes)
+BUILTINPROTO (install)
+BUILTINPROTO (update)
+BUILTINPROTO (make_current_app)
+BUILTINPROTO (uninstall)
+BUILTINPROTO (install_bundle)
+BUILTINPROTO (list)
+BUILTINPROTO (info)
+BUILTINPROTO (run)
+BUILTINPROTO (enter)
+BUILTINPROTO (build_init)
+BUILTINPROTO (build)
+BUILTINPROTO (build_finish)
+BUILTINPROTO (build_sign)
+BUILTINPROTO (build_export)
+BUILTINPROTO (build_bundle)
+BUILTINPROTO (build_import)
+BUILTINPROTO (build_update_repo)
+BUILTINPROTO (export_file)
+BUILTINPROTO (override)
 
 #undef BUILTINPROTO
 

commit 1503f455873445a6acbbe47b4abbfbd10bca87d9

Fix wrong reference to the user installation instead of the system one
This seems like a simple typo introduced in eeaa8320, which is causing
to ignore the system installation when auto completing.

app/flatpak-builtins-info.c
@@ -254,9 +254,9 @@ flatpak_complete_info (FlatpakCompletion *completion)
                 flatpak_complete_word (completion, "%s ", parts[3]);
             }
         }
-      if (user_dir)
+      if (system_dir)
         {
-          g_auto(GStrv) refs = flatpak_dir_find_installed_refs (user_dir, completion->argv[1], NULL, opt_arch,
+          g_auto(GStrv) refs = flatpak_dir_find_installed_refs (system_dir, completion->argv[1], NULL, opt_arch,
                                                                 kinds, &error);
           if (refs == NULL)
             flatpak_completion_debug ("find remote refs error: %s", error->message);