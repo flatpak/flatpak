flatpak/flatpak

commit d48891461475b951394ab0fd361895f49d76bc19

document-portal: factor out common code to validate FD
We're going to reuse this in another location.

document-portal/xdp-main.c
@@ -329,19 +329,14 @@ do_create_doc (struct stat *parent_st_buf, const char *path, gboolean reuse_exis
 }
 
 static gboolean
-validate_fd (int fd,
-             struct stat *st_buf,
-             struct stat *real_parent_st_buf,
-             char *path_buffer,
-             GError **error)
+validate_fd_common (int fd,
+                    struct stat *st_buf,
+                    char *path_buffer,
+                    GError **error)
 {
   g_autofree char *proc_path = NULL;
   ssize_t symlink_size;
-  g_autofree char *dirname = NULL;
-  g_autofree char *name = NULL;
   int fd_flags;
-  glnx_fd_close int dir_fd = -1;
-  struct stat real_st_buf;
 
   proc_path = g_strdup_printf ("/proc/self/fd/%d", fd);
 
@@ -367,13 +362,26 @@ validate_fd (int fd,
     }
 
   path_buffer[symlink_size] = 0;
+  return TRUE;
+}
+
+static gboolean
+validate_parent_dir (const char *path,
+                     struct stat *st_buf,
+                     struct stat *real_parent_st_buf,
+                     GError **error)
+{
+  g_autofree char *dirname = NULL;
+  g_autofree char *name = NULL;
+  glnx_fd_close int dir_fd = -1;
+  struct stat real_st_buf;
 
   /* We open the parent directory and do the stat in that, so that we have
    * trustworthy parent dev/ino for later verification. Otherwise the caller
    * could later replace a parent with a symlink and make us read some other file
    */
-  dirname = g_path_get_dirname (path_buffer);
-  name = g_path_get_basename (path_buffer);
+  dirname = g_path_get_dirname (path);
+  name = g_path_get_basename (path);
   dir_fd = open (dirname, O_CLOEXEC | O_PATH);
 
   if (fstat (dir_fd, real_parent_st_buf) < 0 ||
@@ -387,6 +395,22 @@ validate_fd (int fd,
                    "Invalid fd passed");
       return FALSE;
     }
+
+  return TRUE;
+}
+
+static gboolean
+validate_fd (int fd,
+             struct stat *st_buf,
+             struct stat *real_parent_st_buf,
+             char *path_buffer,
+             GError **error)
+{
+  if (!validate_fd_common (fd, st_buf, path_buffer, error))
+    return FALSE;
+
+  if (!validate_parent_dir (path_buffer, st_buf, real_parent_st_buf, error))
+    return FALSE;
 
   return TRUE;
 }
@@ -506,14 +530,14 @@ portal_add_named (GDBusMethodInvocation *invocation,
   GUnixFDList *fd_list;
   g_autofree char *id = NULL;
   g_autofree char *proc_path = NULL;
-  int parent_fd_id, parent_fd, fds_len, fd_flags;
+  int parent_fd_id, parent_fd, fds_len;
   const int *fds;
   char parent_path_buffer[PATH_MAX + 1];
   g_autofree char *path = NULL;
-  ssize_t symlink_size;
   struct stat parent_st_buf;
   const char *filename;
   gboolean reuse_existing, persistent;
+  g_autoptr(GError) error = NULL;
 
   g_autoptr(GVariant) filename_v = NULL;
 
@@ -548,26 +572,9 @@ portal_add_named (GDBusMethodInvocation *invocation,
       return;
     }
 
-  proc_path = g_strdup_printf ("/proc/self/fd/%d", parent_fd);
-
-  if (parent_fd == -1 ||
-      /* Must be able to get fd flags */
-      (fd_flags = fcntl (parent_fd, F_GETFL)) == -1 ||
-      /* Must be O_PATH */
-      ((fd_flags & O_PATH) != O_PATH) ||
-      /* Must not be O_NOFOLLOW (because we want the target file) */
-      ((fd_flags & O_NOFOLLOW) == O_PATH) ||
-      /* Must be able to fstat */
-      fstat (parent_fd, &parent_st_buf) < 0 ||
-      /* Must be a directory file */
-      (parent_st_buf.st_mode & S_IFMT) != S_IFDIR ||
-      /* Must be able to read path from /proc/self/fd */
-      /* This is an absolute and (at least at open time) symlink-expanded path */
-      (symlink_size = readlink (proc_path, parent_path_buffer, sizeof (parent_path_buffer) - 1)) < 0)
+  if (!validate_fd_common (parent_fd, &parent_st_buf, parent_path_buffer, &error))
     {
-      g_dbus_method_invocation_return_error (invocation,
-                                             FLATPAK_PORTAL_ERROR, FLATPAK_PORTAL_ERROR_INVALID_ARGUMENT,
-                                             "Invalid fd passed");
+      g_dbus_method_invocation_return_gerror (invocation, error);
       return;
     }
 
@@ -578,8 +585,6 @@ portal_add_named (GDBusMethodInvocation *invocation,
                                              "Invalid fd passed");
       return;
     }
-
-  parent_path_buffer[symlink_size] = 0;
 
   path = g_build_filename (parent_path_buffer, filename, NULL);
 

commit 652f64faadccbad6756f082343d366f204e0caed

Fix document portal add_named
This regressed in d48891461475b951394ab0fd361895f49d76bc19 where
it used to require a dir fd, but then required a regular fd.

document-portal/xdp-main.c
@@ -334,6 +334,7 @@ do_create_doc (struct stat *parent_st_buf, const char *path, gboolean reuse_exis
 static gboolean
 validate_fd_common (int fd,
                     struct stat *st_buf,
+                    mode_t st_mode,
                     char *path_buffer,
                     GError **error)
 {
@@ -352,8 +353,8 @@ validate_fd_common (int fd,
       ((fd_flags & O_NOFOLLOW) == O_NOFOLLOW) ||
       /* Must be able to fstat */
       fstat (fd, st_buf) < 0 ||
-      /* Must be a regular file */
-      (st_buf->st_mode & S_IFMT) != S_IFREG ||
+      /* Must be a regular file or directory (depending on use) */
+      (st_buf->st_mode & S_IFMT) != st_mode ||
       /* Must be able to read path from /proc/self/fd */
       /* This is an absolute and (at least at open time) symlink-expanded path */
       (symlink_size = readlink (proc_path, path_buffer, PATH_MAX)) < 0)
@@ -414,7 +415,7 @@ validate_fd (int fd,
   g_autofree char *app_path = NULL;
   g_autofree char *runtime_path = NULL;
 
-  if (!validate_fd_common (fd, st_buf, path_buffer, error))
+  if (!validate_fd_common (fd, st_buf, S_IFREG, path_buffer, error))
     return FALSE;
 
   /* For apps we translate /app and /usr to the installed locations.
@@ -608,7 +609,7 @@ portal_add_named (GDBusMethodInvocation *invocation,
       return;
     }
 
-  if (!validate_fd_common (parent_fd, &parent_st_buf, parent_path_buffer, &error))
+  if (!validate_fd_common (parent_fd, &parent_st_buf, S_IFDIR, parent_path_buffer, &error))
     {
       g_dbus_method_invocation_return_gerror (invocation, error);
       return;