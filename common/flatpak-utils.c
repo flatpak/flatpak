bd9ff4de31e41490e38b465d1f810bde01511d81
Alexander Larsson <alexl@redhat.com>
2017-02-02 13:56:34 +0100

    extensions: Support merge-dirs=list..
    This causes the listed directories to become symlink farms
    into all the subdirectory extensions.
    
    (cherry picked from commit c88f1aa8350e197112362517f0486fafa7e0ae07)

--- common/flatpak-utils.c
+++ common/flatpak-utils.c
@@ -3452,6 +3452,7 @@ flatpak_extension_free (FlatpakExtension *extension)
   g_free (extension->files_path);
   g_free (extension->add_ld_path);
   g_free (extension->subdir_suffix);
+  g_strfreev (extension->merge_dirs);
   g_free (extension);
 }
 
@@ -3472,6 +3473,7 @@ flatpak_extension_new (const char *id,
                        const char *directory,
                        const char *add_ld_path,
                        const char *subdir_suffix,
+                       char **merge_dirs,
                        GFile *files,
                        gboolean is_unmaintained)
 {
@@ -3484,6 +3486,7 @@ flatpak_extension_new (const char *id,
   ext->files_path = g_file_get_path (files);
   ext->add_ld_path = g_strdup (add_ld_path);
   ext->subdir_suffix = g_strdup (subdir_suffix);
+  ext->merge_dirs = g_strdupv (merge_dirs);
   ext->is_unmaintained = is_unmaintained;
 
   if (is_unmaintained)
@@ -3526,6 +3529,7 @@ flatpak_list_extensions (GKeyFile   *metakey,
           g_autofree char *directory = g_key_file_get_string (metakey, groups[i], "directory", NULL);
           g_autofree char *version = g_key_file_get_string (metakey, groups[i], "version", NULL);
           g_autofree char *add_ld_path = g_key_file_get_string (metakey, groups[i], "add-ld-path", NULL);
+          g_auto(GStrv) merge_dirs = g_key_file_get_string_list (metakey, groups[i], "merge-dirs", NULL, NULL);
           g_autofree char *subdir_suffix = g_key_file_get_string (metakey, groups[i], "subdirectory-suffix", NULL);
           g_autofree char *ref = NULL;
           const char *branch;
@@ -3552,7 +3556,7 @@ flatpak_list_extensions (GKeyFile   *metakey,
           /* Prefer a full extension (org.freedesktop.Locale) over subdirectory ones (org.freedesktop.Locale.sv) */
           if (files != NULL)
             {
-              ext = flatpak_extension_new (extension, extension, ref, directory, add_ld_path, subdir_suffix, files, is_unmaintained);
+              ext = flatpak_extension_new (extension, extension, ref, directory, add_ld_path, subdir_suffix, merge_dirs, files, is_unmaintained);
               res = g_list_prepend (res, ext);
             }
           else if (g_key_file_get_boolean (metakey, groups[i],
@@ -3573,7 +3577,7 @@ flatpak_list_extensions (GKeyFile   *metakey,
 
                   if (subdir_files)
                     {
-                      ext = flatpak_extension_new (extension, refs[j], dir_ref, extended_dir, add_ld_path, subdir_suffix, subdir_files, FALSE);
+                      ext = flatpak_extension_new (extension, refs[j], dir_ref, extended_dir, add_ld_path, subdir_suffix, merge_dirs, subdir_files, FALSE);
                       ext->needs_tmpfs = TRUE;
                       res = g_list_prepend (res, ext);
                     }
@@ -3589,7 +3593,7 @@ flatpak_list_extensions (GKeyFile   *metakey,
 
                   if (subdir_files)
                     {
-                      ext = flatpak_extension_new (extension, unmaintained_refs[j], dir_ref, extended_dir, add_ld_path, subdir_suffix, subdir_files, TRUE);
+                      ext = flatpak_extension_new (extension, unmaintained_refs[j], dir_ref, extended_dir, add_ld_path, subdir_suffix, merge_dirs, subdir_files, TRUE);
                       ext->needs_tmpfs = TRUE;
                       res = g_list_prepend (res, ext);
                     }
