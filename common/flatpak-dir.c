0bf1b3125612f0b1f56ff00b856f9547f3c28e64
Alexander Larsson <alexl@redhat.com>
2017-05-09 16:33:47 +0200

    update_remote_configuration_for_summary: Never use system-helper
    The system-helper support now happens at the higher level
    in update_remote_configuration, so there is no need for this.

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -7825,30 +7825,6 @@ flatpak_dir_update_remote_configuration_for_summary (FlatpakDir   *self,
     if (dry_run || !has_changed)
       return TRUE;
 
-    if (flatpak_dir_use_system_helper (self, NULL))
-      {
-        FlatpakSystemHelper *system_helper;
-        g_autofree char *config_data = g_key_file_to_data (config, NULL, NULL);
-        g_autoptr(GVariant) gpg_data_v = NULL;
-        const char *installation = flatpak_dir_get_id (self);
-
-        gpg_data_v = g_variant_ref_sink (g_variant_new_from_data (G_VARIANT_TYPE ("ay"), "", 0, TRUE, NULL, NULL));
-
-        system_helper = flatpak_dir_get_system_helper (self);
-        g_assert (system_helper != NULL);
-
-        g_debug ("Calling system helper: ConfigureRemote");
-        if (!flatpak_system_helper_call_configure_remote_sync (system_helper,
-                                                               0, remote,
-                                                               config_data,
-                                                               gpg_data_v,
-                                                               installation ? installation : "",
-                                                               cancellable, error))
-          return FALSE;
-
-        return TRUE;
-      }
-
     /* Update the local remote configuration with the updated info. */
     if (!flatpak_dir_modify_remote (self, remote, config, NULL, cancellable, error))
       return FALSE;
