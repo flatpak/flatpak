4cb47a21125c186486b3a045603530ef7c770dcf
Philip Withnall <withnall@endlessm.com>
2017-06-30 12:08:00 +0100

    tests: Add a test for unsigned summary support
    This relies on peer to peer support also being enabled; the test is
    skipped otherwise.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- tests/libtest.sh
+++ tests/libtest.sh
@@ -284,6 +284,13 @@ skip_without_python2 () {
     fi
 }
 
+skip_without_p2p () {
+    if ! ${FLATPAK} remote-add --help | grep -q -e '--collection-id'; then
+        echo "1..0 # SKIP this test requires peer to peer support (--enable-p2p)"
+        exit 0
+    fi
+}
+
 
 sed s#@testdir@#${test_builddir}# ${test_srcdir}/session.conf.in > session.conf
 dbus-daemon --fork --config-file=session.conf --print-address=3 --print-pid=4 \
