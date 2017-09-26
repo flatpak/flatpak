ea8ba7597ef6b51c0fdd1c7123e394048e0ed3dd
Alexander Larsson <alexl@redhat.com>
2017-09-26 10:53:00 +0200

    CI: Use exactly ostree 2017.11

--- ci/build.sh
+++ ci/build.sh
@@ -13,8 +13,8 @@ pkg_install sudo which attr fuse \
     /usr/bin/{update-mime-database,update-desktop-database,gtk-update-icon-cache}
 pkg_install_if_os fedora gjs parallel clang
 
-# Temporarily build ostree from git master for https://github.com/flatpak/flatpak/pull/848
-(git clone --depth=1 https://github.com/ostreedev/ostree/
+# Use ostree minimal dependency
+(git clone -b  v2017.11 --depth=1 https://github.com/ostreedev/ostree/
  cd ostree
  pkg_install_builddeps ostree
  unset CFLAGS # the sanitizers require calling apps be linked too
