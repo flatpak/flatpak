c6fdfa0ad4a41d6bfe7541c965383aa18f5f75f8
Alexander Larsson <alexl@redhat.com>
2017-10-18 17:54:14 +0200

    fixup! Import CI setup from master

--- ci/build.sh
+++ ci/build.sh
@@ -8,9 +8,8 @@ dn=$(dirname $0)
 
 pkg_install sudo which attr fuse \
     libubsan libasan libtsan elfutils-libelf-devel libdwarf-devel \
-    elfutils git gettext-devel \
+    elfutils git gettext-devel ostree-devel ostree \
     /usr/bin/{update-mime-database,update-desktop-database,gtk-update-icon-cache}
-pkg_install_testing ostree-devel ostree
 pkg_install_if_os fedora gjs parallel clang
 pkg_install_builddeps flatpak
 
