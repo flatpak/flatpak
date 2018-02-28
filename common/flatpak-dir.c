ebcb5e3fbe82ff3b3d0b14a4db194c57e9e7c75c
Philip Withnall <withnall@endlessm.com>
2018-02-28 11:12:37 +0000

    common: Include arch in error message to clarify things
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://github.com/flatpak/flatpak/issues/1424
    
    Closes: #1462
    Approved by: mwleeds

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -8469,7 +8469,10 @@ flatpak_dir_find_installed_ref (FlatpakDir   *self,
     }
 
   g_set_error (error, FLATPAK_ERROR, FLATPAK_ERROR_NOT_INSTALLED,
-               _("%s %s not installed"), opt_name ? opt_name : "*unspecified*", opt_branch ? opt_branch : "master");
+               _("%s/%s/%s not installed"),
+               opt_name ? opt_name : "*unspecified*",
+               opt_arch ? opt_arch : "*unspecified*",
+               opt_branch ? opt_branch : "*unspecified*");
   return NULL;
 }
 
