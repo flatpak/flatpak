flatpak/flatpak

commit 20c8420128e27135e076b4fc3554f4c4de558f4d

update: Fix updates of non-primary arches
When updating with no arch is specified we now update
appdata for all supported arches, and we don't look for
updates only of the primary arch.

app/flatpak-builtins-update.c
@@ -79,6 +79,8 @@ flatpak_builtin_update (int           argc,
   const char *default_branch = NULL;
   FlatpakKinds kinds;
   g_autoptr(GPtrArray) transactions = NULL;
+  const char *opt_arches[2] = {NULL, NULL};
+  const char **arches = flatpak_get_arches ();
 
   context = g_option_context_new (_("[REF...] - Update applications or runtimes"));
   g_option_context_set_translation_domain (context, GETTEXT_PACKAGE);
@@ -88,11 +90,20 @@ flatpak_builtin_update (int           argc,
                                      &dirs, cancellable, error))
     return FALSE;
 
-  if (opt_arch == NULL)
-    opt_arch = (char *)flatpak_get_arch ();
+  if (opt_arch)
+    {
+      opt_arches[0] = opt_arch;
+      arches = opt_arches;
+    }
 
   if (opt_appstream)
-    return update_appstream (dirs, argc >= 2 ? argv[1] : NULL, opt_arch, 0, FALSE, cancellable, error);
+    {
+      for (i = 0; arches[i] != NULL; i++)
+        {
+          if (!update_appstream (dirs, argc >= 2 ? argv[1] : NULL, arches[i], 0, FALSE, cancellable, error))
+            return FALSE;
+        }
+    }
 
   prefs = &argv[1];
   n_prefs = argc - 1;
@@ -229,7 +240,13 @@ flatpak_builtin_update (int           argc,
     }
 
   if (n_prefs == 0)
-    return update_appstream (dirs, NULL, opt_arch, 0, FALSE, cancellable, error);
+    {
+      for (i = 0; arches[i] != NULL; i++)
+        {
+          if (!update_appstream (dirs, NULL, arches[i], FLATPAK_APPSTREAM_TTL, TRUE, cancellable, error))
+            return FALSE;
+        }
+    }
 
   return TRUE;
 }

app/flatpak-builtins-search.c
@@ -29,9 +29,6 @@
 #include "flatpak-table-printer.h"
 #include "flatpak-utils.h"
 
-/* Appstream data expires after a day */
-#define FLATPAK_APPSTREAM_TTL 86400
-
 static GPtrArray *
 get_remote_stores (GPtrArray *dirs, GCancellable *cancellable)
 {

app/flatpak-builtins-utils.h
@@ -26,6 +26,9 @@
 #include "flatpak-utils.h"
 #include "flatpak-dir.h"
 
+/* Appstream data expires after a day */
+#define FLATPAK_APPSTREAM_TTL 86400
+
 gboolean    looks_like_branch (const char  *branch);
 GBytes *    download_uri      (const char  *url,
                                GError     **error);

commit 280d881d2df6fd2c5976ce84f3b0bd07ffa4368b

update: Don't do a full update given --appstream
Until recently, "flatpak update --appstream" caused flatpak to only
update appstream data. Then commit 20c842012 accidentally made flatpak
also do a full update after updating appstream data, so this commit
fixes the regression.

app/flatpak-builtins-update.c
@@ -103,6 +103,8 @@ flatpak_builtin_update (int           argc,
           if (!update_appstream (dirs, argc >= 2 ? argv[1] : NULL, arches[i], 0, FALSE, cancellable, error))
             return FALSE;
         }
+
+      return TRUE;
     }
 
   prefs = &argv[1];