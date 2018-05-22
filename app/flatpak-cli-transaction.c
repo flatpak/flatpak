cca9005a3acf7da1e695b461e85786bb9c6e3abe
Alexander Larsson <alexl@redhat.com>
2018-05-22 11:53:00 +0200

    app: Add flatpak cli constructor
    This lets us centralize in one place all setup needed for cli use
    of FlatpakTransactions (like interaction and progress).

---
+++ app/flatpak-cli-transaction.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2018 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *       Alexander Larsson <alexl@redhat.com>
+ */
+
+#include "config.h"
+
+#include "flatpak-cli-transaction.h"
+#include "flatpak-transaction-private.h"
+
+FlatpakTransaction *
+flatpak_cli_transaction_new (FlatpakDir *dir,
+                             gboolean disable_interaction)
+{
+  FlatpakTransaction *transaction = flatpak_transaction_new (dir);
+  flatpak_transaction_set_disable_interaction (transaction, disable_interaction);
+  return transaction;
+}
