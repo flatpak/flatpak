1172747346f39950b17ecfca43c0a796f9b630e5
Alexander Larsson <alexl@redhat.com>
2018-05-24 11:09:13 +0200

    Transaction: Add signal for choosing a remote when needed
    Also move the existing code for this into FlatpakCliTransaction

--- app/flatpak-cli-transaction.c
+++ app/flatpak-cli-transaction.c
@@ -22,12 +22,75 @@
 
 #include "flatpak-cli-transaction.h"
 #include "flatpak-transaction-private.h"
+#include "flatpak-utils-private.h"
+#include <glib/gi18n.h>
+
+typedef struct {
+  FlatpakTransaction *transaction;
+  gboolean disable_interaction;
+} FlatpakCliTransaction;
+
+static int
+choose_remote_for_ref (FlatpakTransaction *transaction,
+                       const char *for_ref,
+                       const char *runtime_ref,
+                       const char * const *remotes,
+                       gpointer data)
+{
+  FlatpakCliTransaction *cli = data;
+  int n_remotes = g_strv_length ((char **)remotes);
+  int chosen = -1;
+  const char *pref;
+  int i;
+
+  pref = strchr (for_ref, '/') + 1;
+
+  if (cli->disable_interaction)
+    {
+      g_print (_("Required runtime for %s (%s) found in remote %s\n"),
+               pref, runtime_ref, remotes[0]);
+      chosen = 0;
+    }
+  else if (n_remotes == 1)
+    {
+      g_print (_("Required runtime for %s (%s) found in remote %s\n"),
+               pref, runtime_ref, remotes[0]);
+      if (flatpak_yes_no_prompt (_("Do you want to install it?")))
+        chosen = 0;
+    }
+  else
+    {
+      g_print (_("Required runtime for %s (%s) found in remotes: %s\n"),
+               pref, runtime_ref, remotes[0]);
+      for (i = 0; remotes[i] != NULL; i++)
+        {
+          g_print ("%d) %s\n", i + 1, remotes[i]);
+        }
+      chosen = flatpak_number_prompt (0, n_remotes, _("Which do you want to install (0 to abort)?"));
+      chosen -= 1; /* convert from base-1 to base-0 (and -1 to abort) */
+    }
+
+  return chosen;
+}
+
+static void
+flatpak_cli_transaction_free (FlatpakCliTransaction *cli)
+{
+  g_free (cli);
+}
 
 FlatpakTransaction *
 flatpak_cli_transaction_new (FlatpakDir *dir,
                              gboolean disable_interaction)
 {
   FlatpakTransaction *transaction = flatpak_transaction_new (dir);
-  flatpak_transaction_set_disable_interaction (transaction, disable_interaction);
+  FlatpakCliTransaction *cli = g_new0 (FlatpakCliTransaction, 1);
+
+  cli->transaction = transaction;
+  cli->disable_interaction = disable_interaction;
+  g_object_set_data_full (G_OBJECT (transaction), "cli", cli, (GDestroyNotify)flatpak_cli_transaction_free);
+
+  g_signal_connect (transaction, "choose-remote-for-ref", G_CALLBACK (choose_remote_for_ref), cli);
+
   return transaction;
 }
