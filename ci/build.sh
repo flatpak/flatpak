2d9865d1d8af9e02de7e99f4e58dd0ce30f3d712
Alexander Larsson <alexl@redhat.com>
2018-05-31 14:35:13 +0200

    CI: Install python2
    This is needed for the tests
    
    Closes: #1741
    Approved by: alexlarsson

--- ci/build.sh
+++ ci/build.sh
@@ -11,7 +11,7 @@ pkg_install sudo which attr fuse bison \
     elfutils git gettext-devel libappstream-glib-devel \
     /usr/bin/{update-mime-database,update-desktop-database,gtk-update-icon-cache}
 pkg_install_testing ostree-devel ostree
-pkg_install_if_os fedora gjs parallel clang
+pkg_install_if_os fedora gjs parallel clang python2
 pkg_install_builddeps flatpak
 
 build --enable-gtk-doc ${CONFIGOPTS:-}
