e818097aea5305fc38e0ebba2299f3d304993922
Alexander Larsson <alexl@redhat.com>
2018-05-31 17:13:38 +0200

    repair: Update copyright year to 2018
    LIVE IN THE NOW!

--- app/flatpak-builtins-repair.c
+++ app/flatpak-builtins-repair.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017 Red Hat, Inc
+ * Copyright © 2018 Red Hat, Inc
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
