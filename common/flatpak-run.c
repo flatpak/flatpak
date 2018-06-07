59ecad0f2932565bc55e8d156e9788376d2680e5
Joonas Sarajärvi <muep@iki.fi>
2018-06-07 21:39:20 +0300

    Add ssh-auth socket support
    SSH authentication sockets can be placed in a number of places, so it
    is difficult for applications to just mount a fixed directory or
    directories, hoping that SSH_AUTH_SOCK points somewhere inside the
    mounted content.
    
    Closes: #1764
    Approved by: alexlarsson

--- common/flatpak-run.c
+++ common/flatpak-run.c
@@ -252,6 +252,32 @@ flatpak_run_add_wayland_args (FlatpakBwrap *bwrap)
   return res;
 }
 
+static void
+flatpak_run_add_ssh_args(FlatpakBwrap *bwrap)
+{
+  const char * auth_socket;
+  g_autofree char * sandbox_auth_socket = NULL;
+
+  auth_socket = g_getenv ("SSH_AUTH_SOCK");
+
+  if (!auth_socket)
+    return; /* ssh agent not present */
+
+  if (!g_file_test (auth_socket, G_FILE_TEST_EXISTS))
+    {
+      /* Let's clean it up, so that the application will not try to connect */
+      flatpak_bwrap_unset_env(bwrap, "SSH_AUTH_SOCK");
+      return;
+    }
+
+  sandbox_auth_socket = g_strdup_printf ("/run/user/%d/ssh-auth", getuid());
+
+  flatpak_bwrap_add_args (bwrap,
+                          "--bind", auth_socket, sandbox_auth_socket,
+                          NULL);
+  flatpak_bwrap_set_env (bwrap, "SSH_AUTH_SOCK", sandbox_auth_socket, TRUE);
+}
+
 /* Try to find a default server from a pulseaudio confguration file */
 static char *
 flatpak_run_get_pulseaudio_server_user_config (const char *path)
@@ -1056,6 +1082,10 @@ flatpak_run_add_environment_args (FlatpakBwrap   *bwrap,
     allow_x11 = (context->sockets & FLATPAK_CONTEXT_SOCKET_X11) != 0;
 
   flatpak_run_add_x11_args (bwrap, allow_x11);
+
+  if (context->sockets & FLATPAK_CONTEXT_SOCKET_SSH_AUTH) {
+    flatpak_run_add_ssh_args (bwrap);
+  }
 
   if (context->sockets & FLATPAK_CONTEXT_SOCKET_PULSEAUDIO)
     {
