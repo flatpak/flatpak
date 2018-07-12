6486da9133e6598f3eb017cfb0038b65c2706e1d
Owen W. Taylor <otaylor@fishsoup.net>
2018-07-12 18:13:11 +0200

    Cache the downloaded registry index in compressed form
    The OCI index information should be highly compressable (especially if
    icons are remote URI's rather than data URI's) so downloading it and
    storing it compressed will provide sigificant efficiency gains.

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -1019,7 +1019,7 @@ flatpak_dir_get_oci_index_location (FlatpakDir *self,
                                     const char *remote,
                                     GError    **error)
 {
-  return flatpak_dir_get_oci_cache_file (self, remote, ".index", error);
+  return flatpak_dir_get_oci_cache_file (self, remote, ".index.gz", error);
 }
 
 static GFile *
@@ -1053,7 +1053,7 @@ flatpak_dir_remove_oci_files (FlatpakDir   *self,
                               GCancellable *cancellable,
                               GError      **error)
 {
-  if (!flatpak_dir_remove_oci_file (self, remote, ".index", cancellable, error) ||
+  if (!flatpak_dir_remove_oci_file (self, remote, ".index.gz", cancellable, error) ||
       !flatpak_dir_remove_oci_file (self, remote, ".summary", cancellable, error))
     return FALSE;
 
