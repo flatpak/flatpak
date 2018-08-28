c607f75b727c3cd8db45aa80621ec0e3d6337c78
Matthias Clasen <mclasen@redhat.com>
2018-08-28 13:01:21 -0400

    Tell bubblewrap to write info
    Create a bwrapinfo.json file and tell bubblewrap
    to write its 'info' there. For now, this just contains
    the child-pid. More may appear over time.

--- common/flatpak-run.c
+++ common/flatpak-run.c
@@ -1689,7 +1689,8 @@ flatpak_run_add_app_info_args (FlatpakBwrap   *bwrap,
                                GError        **error)
 {
   g_autofree char *info_path = NULL;
-  int fd, fd2;
+  g_autofree char *bwrapinfo_path = NULL;
+  int fd, fd2, fd3;
 
   g_autoptr(GKeyFile) keyfile = NULL;
   g_autofree char *runtime_path = NULL;
@@ -1843,7 +1844,21 @@ flatpak_run_add_app_info_args (FlatpakBwrap   *bwrap,
                           "--symlink", "../../../.flatpak-info", old_dest,
                           NULL);
 
-  if (app_info_path_out != NULL)
+  bwrapinfo_path = g_build_filename (instance_id_host_dir, "bwrapinfo.json", NULL);
+  fd3 = open (bwrapinfo_path, O_RDWR | O_CREAT, 0644);
+  if (fd3 == -1)
+    {
+      close (fd);
+      close (fd2);
+      int errsv = errno;
+      g_set_error (error, G_IO_ERROR, g_io_error_from_errno (errsv),
+                   _("Failed to open brwapinfo.json file: %s"), g_strerror (errsv));
+      return FALSE;
+    }
+
+  flatpak_bwrap_add_args_data_fd (bwrap, "--info-fd", fd3, NULL);
+
+ if (app_info_path_out != NULL)
     *app_info_path_out = g_strdup_printf ("/proc/self/fd/%d", fd);
 
   if (instance_id_host_dir_out != NULL)
