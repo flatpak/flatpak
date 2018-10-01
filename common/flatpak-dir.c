4c65bd653950eb9aaafaec282c71967e67e17b63
Robert McQueen <rob@endlessm.com>
2018-10-01 11:59:10 +0100

    dir: use g_utime in flatpak_dir_mark_changed
    Avoids an un-necessary fsync() call of an empty file.

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -31,6 +31,7 @@
 #include <glnx-console.h>
 
 #include <glib/gi18n.h>
+#include <glib/gstdio.h>
 
 #include <libxml/parser.h>
 #include <libxml/tree.h>
@@ -2456,10 +2457,19 @@ flatpak_dir_mark_changed (FlatpakDir *self,
                           GError    **error)
 {
   g_autoptr(GFile) changed_file = NULL;
+  g_autofree char * changed_path = NULL;
 
   changed_file = flatpak_dir_get_changed_path (self);
+  changed_path = g_file_get_path (changed_file);
+
+  if (!g_utime (changed_path, NULL))
+    return TRUE;
+
+  if (errno != ENOENT)
+    return glnx_throw_errno (error);
+
   if (!g_file_replace_contents (changed_file, "", 0, NULL, FALSE,
-                                G_FILE_CREATE_REPLACE_DESTINATION, NULL, NULL, error))
+                                G_FILE_CREATE_NONE, NULL, NULL, error))
     return FALSE;
 
   return TRUE;
