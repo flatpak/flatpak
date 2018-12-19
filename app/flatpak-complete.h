80e2bc5f894983688dc71cd9bb91a70dfb456149
Matthias Clasen <mclasen@redhat.com>
2018-12-18 23:23:22 -0500

    completion: Add an api to complete columns
    The new function, flatpak_compete_columns, takes an array
    of Column structs and completes a --columns= option.
    
    Unfortunately, bash completion does not let us do better
    than repeating the entire list of columns for every
    possible completion. This is not ideal, but at least
    it gives good results for the first column.

--- app/flatpak-complete.h
+++ app/flatpak-complete.h
@@ -23,6 +23,7 @@
 
 #include <ostree.h>
 #include "flatpak-dir-private.h"
+#include "flatpak-builtins-utils.h"
 
 struct FlatpakCompletion
 {
@@ -58,6 +59,8 @@ void               flatpak_complete_file (FlatpakCompletion *completion,
 void               flatpak_complete_dir (FlatpakCompletion *completion);
 void               flatpak_complete_options (FlatpakCompletion *completion,
                                              GOptionEntry      *entries);
+void               flatpak_complete_columns (FlatpakCompletion *completion,
+                                             Column            *columns);
 void               flatpak_completion_free (FlatpakCompletion *completion);
 void               flatpak_complete_context (FlatpakCompletion *completion);
 
