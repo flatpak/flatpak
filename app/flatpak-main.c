2eb5eb17ec7958daf93312a9bb15555f48f23568
Matthias Clasen <mclasen@redhat.com>
2018-12-30 15:11:52 -0500

    Tweak the docs for --user/system/--installation
    Take out the plurals, and tweak the wording to make
    it less misleading.
    
    See: #2509

--- app/flatpak-main.c
+++ app/flatpak-main.c
@@ -164,9 +164,9 @@ static GOptionEntry empty_entries[] = {
 };
 
 GOptionEntry user_entries[] = {
-  { "user", 0, 0, G_OPTION_ARG_NONE, &opt_user, N_("Work on user installations"), NULL },
-  { "system", 0, 0, G_OPTION_ARG_NONE, &opt_system, N_("Work on system-wide installations (default)"), NULL },
-  { "installation", 0, 0, G_OPTION_ARG_STRING_ARRAY, &opt_installations, N_("Work on specific system-wide installation(s)"), N_("NAME") },
+  { "user", 0, 0, G_OPTION_ARG_NONE, &opt_user, N_("Work on the user installation"), NULL },
+  { "system", 0, 0, G_OPTION_ARG_NONE, &opt_system, N_("Work on the system-wide installation (default)"), NULL },
+  { "installation", 0, 0, G_OPTION_ARG_STRING_ARRAY, &opt_installations, N_("Work on a non-default system-wide installation"), N_("NAME") },
   { NULL }
 };
 
