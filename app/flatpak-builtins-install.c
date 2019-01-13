e92c3f42cb13b38ccfb58ef27f981097843ce633
Matthias Clasen <mclasen@redhat.com>
2019-01-13 11:43:59 -0500

    install: Don't fail completion on empty repos
    This lets completion keep working after you type
    flatpak install --installation=empty
    It is somewhat cosmetic since there's nothing to
    install on an empty repo, but no need to silently
    disappear the remaining options.

--- app/flatpak-builtins-install.c
+++ app/flatpak-builtins-install.c
@@ -538,7 +538,8 @@ flatpak_complete_install (FlatpakCompletion *completion)
 
   context = g_option_context_new ("");
   if (!flatpak_option_context_parse (context, options, &completion->argc, &completion->argv,
-                                     FLATPAK_BUILTIN_FLAG_ONE_DIR, &dirs, NULL, NULL))
+                                     FLATPAK_BUILTIN_FLAG_ONE_DIR | FLATPAK_BUILTIN_FLAG_OPTIONAL_REPO,
+                                     &dirs, NULL, NULL))
     return FALSE;
 
   dir = g_ptr_array_index (dirs, 0);
