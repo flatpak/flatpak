98e318c5f7ead11b33fab7c7834cdc7ee24d3091
Matthias Clasen <mclasen@redhat.com>
2019-01-15 15:14:06 -0500

    Move a function
    flatpak_context_load_for_deploy() was defined in
    flatpak-context-private.h, but implemented in flatpak-run.c.
    Clearly, and oversight.

--- common/flatpak-context.c
+++ common/flatpak-context.c
@@ -2201,3 +2201,22 @@ flatpak_context_append_bwrap_filesystem (FlatpakContext  *context,
   if (exports_out)
     *exports_out = g_steal_pointer (&exports);
 }
+
+FlatpakContext *
+flatpak_context_load_for_deploy (FlatpakDeploy *deploy,
+                                 GError       **error)
+{
+  g_autoptr(FlatpakContext) context = NULL;
+  g_autoptr(FlatpakContext) overrides = NULL;
+  g_autoptr(GKeyFile) metakey = NULL;
+
+  metakey = flatpak_deploy_get_metadata (deploy);
+  context = flatpak_app_compute_permissions (metakey, NULL, error);
+  if (context == NULL)
+    return NULL;
+
+  overrides = flatpak_deploy_get_overrides (deploy);
+  flatpak_context_merge (context, overrides);
+
+  return g_steal_pointer (&context);
+}
