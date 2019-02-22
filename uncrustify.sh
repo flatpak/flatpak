2fbc6227c12cb65de7c50033a81a8e121f4e63cc
Matthew Leeds <matthew.leeds@endlessm.com>
2019-02-21 23:22:27 -0800

    uncrustify: Exclude app/flatpak-polkit-agent-text-listener.[ch]
    These are copied from polkit so we don't want to uncrustify them.

--- uncrustify.sh
+++ uncrustify.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-uncrustify -c uncrustify.cfg --no-backup `git ls-tree --name-only -r HEAD | grep \\\.[ch]$ | grep -v common/valgrind-private.h`
+uncrustify -c uncrustify.cfg --no-backup `git ls-tree --name-only -r HEAD | grep \\\.[ch]$ | grep -v common/valgrind-private.h | grep -v app/flatpak-polkit-agent-text-listener\\\.[ch]`
