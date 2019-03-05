cbc61a152b90bbd0031c654ace3e2e72c4738a6e
Matthew Leeds <matthew.leeds@endlessm.com>
2019-03-05 13:04:51 -0800

    Consistently use flatpak_load_http_uri()
    Currently Flatpak has a few different implementations of helper
    functions to download a URI using libsoup, but the best one seems to be
    in common/flatpak-utils-http.c. So this commit deletes the others and
    makes use of flatpak_load_http_uri() in place of download_uri() in a
    few places. This has a couple consequences:
    1) It means that we're now properly checking HTTP status codes rather
    than assuming that the request was successful, in the install command,
    remote-add command, and in FlatpakTransaction. This fixes an assertion
    failure seen by a user when they tried to use a flatpakref URL that hit
    a 404.
    2) It means that in the places where we're using flatpak_load_http_uri()
    we are only supporting http:// and https:// URLs not, say, file:// ones.
    For the install and remote-add commands this was already being enforced.
    For the handling of flatpakref files and bundles in FlatpakTransaction,
    I believe it's just convention because it doesn't make much sense to
    do anything else; this commit enforces that convention.
    
    Also, add a unit test for the case of trying to install a flatpakref at
    a URL that hits a 404 error.
    
    Fixes https://github.com/flatpak/flatpak/issues/2727

--- app/flatpak-builtins-utils.c
+++ app/flatpak-builtins-utils.c
@@ -92,65 +92,6 @@ looks_like_branch (const char *branch)
   return TRUE;
 }
 
-static SoupSession *
-get_soup_session (void)
-{
-  static SoupSession *soup_session = NULL;
-
-  if (soup_session == NULL)
-    {
-      const char *http_proxy;
-
-      soup_session = soup_session_new_with_options (SOUP_SESSION_USER_AGENT, "flatpak-builder ",
-                                                    SOUP_SESSION_SSL_USE_SYSTEM_CA_FILE, TRUE,
-                                                    SOUP_SESSION_USE_THREAD_CONTEXT, TRUE,
-                                                    SOUP_SESSION_TIMEOUT, 60,
-                                                    SOUP_SESSION_IDLE_TIMEOUT, 60,
-                                                    NULL);
-      http_proxy = g_getenv ("http_proxy");
-      if (http_proxy)
-        {
-          g_autoptr(SoupURI) proxy_uri = soup_uri_new (http_proxy);
-          if (!proxy_uri)
-            g_warning ("Invalid proxy URI '%s'", http_proxy);
-          else
-            g_object_set (soup_session, SOUP_SESSION_PROXY_URI, proxy_uri, NULL);
-        }
-    }
-
-  return soup_session;
-}
-
-GBytes *
-download_uri (const char *url,
-              GError    **error)
-{
-  SoupSession *session;
-  g_autoptr(SoupRequest) req = NULL;
-  g_autoptr(GInputStream) input = NULL;
-  g_autoptr(GOutputStream) out = NULL;
-
-  session = get_soup_session ();
-
-  req = soup_session_request (session, url, error);
-  if (req == NULL)
-    return NULL;
-
-  input = soup_request_send (req, NULL, error);
-  if (input == NULL)
-    return NULL;
-
-  out = g_memory_output_stream_new_resizable ();
-  if (!g_output_stream_splice (out,
-                               input,
-                               G_OUTPUT_STREAM_SPLICE_CLOSE_TARGET | G_OUTPUT_STREAM_SPLICE_CLOSE_SOURCE,
-                               NULL,
-                               error))
-    return NULL;
-
-  return g_memory_output_stream_steal_as_bytes (G_MEMORY_OUTPUT_STREAM (out));
-}
-
 FlatpakDir *
 flatpak_find_installed_pref (const char *pref, FlatpakKinds kinds, const char *default_arch, const char *default_branch,
                              gboolean search_all, gboolean search_user, gboolean search_system, char **search_installations,
