8c68f00ddbd71e392c87de2c6056e00f0c457498
Alexander Larsson <alexl@redhat.com>
2019-04-12 08:36:43 +0200

    fixup! Add support for PCSC socket

--- common/flatpak-run.c
+++ common/flatpak-run.c
@@ -291,19 +291,20 @@ flatpak_run_add_pcsc_args (FlatpakBwrap *bwrap)
   const char * sandbox_pcsc_socket = "/run/pcscd/pcscd.comm";
 
   pcsc_socket = g_getenv ("PCSCLITE_CSOCK_NAME");
-  if (pcsc_socket) {
-    if (!g_file_test (pcsc_socket, G_FILE_TEST_EXISTS))
-      {
-	flatpak_bwrap_unset_env (bwrap, "PCSCLITE_CSOCK_NAME");
-	return;
-      }
-  } else {
-    pcsc_socket = "/run/pcscd/pcscd.comm";
-    if (!g_file_test (pcsc_socket, G_FILE_TEST_EXISTS))
-      {
-	return;
-      }
-  }
+  if (pcsc_socket)
+    {
+      if (!g_file_test (pcsc_socket, G_FILE_TEST_EXISTS))
+        {
+          flatpak_bwrap_unset_env (bwrap, "PCSCLITE_CSOCK_NAME");
+          return;
+        }
+    }
+  else
+    {
+      pcsc_socket = "/run/pcscd/pcscd.comm";
+      if (!g_file_test (pcsc_socket, G_FILE_TEST_EXISTS))
+        return;
+    }
 
   flatpak_bwrap_add_args (bwrap,
                           "--ro-bind", pcsc_socket, sandbox_pcsc_socket,
