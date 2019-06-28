9f072ce4bf1dc5c9754794d2032d86711061b671
Philip Withnall <withnall@endlessm.com>
2019-06-28 13:45:59 +0100

    ci: Build with malcontent enabled
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- ci/build.sh
+++ ci/build.sh
@@ -9,10 +9,19 @@ dn=$(dirname $0)
 pkg_install sudo which attr fuse bison dbus-daemon \
     libubsan libasan libtsan clang python2 \
     elfutils git gettext-devel libappstream-glib-devel hicolor-icon-theme \
-    dconf-devel fuse-devel \
+    dconf-devel fuse-devel meson dbus-devel \
     /usr/bin/{update-mime-database,update-desktop-database,gtk-update-icon-cache}
 pkg_install_testing ostree-devel ostree
 pkg_install gdk-pixbuf2-modules # needed to make icon validation work
 pkg_install_builddeps flatpak
+
+# malcontent isn’t packaged for our CI distributions yet
+git clone https://gitlab.freedesktop.org/pwithnall/malcontent.git /tmp/malcontent
+pushd /tmp/malcontent
+git checkout tags/0.4.0
+meson setup --prefix=/usr _build
+ninja -C _build
+ninja -C _build install
+popd
 
 build --enable-gtk-doc ${CONFIGOPTS:-}
