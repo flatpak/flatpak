866ba643d2d80cd1c8983aab47b774fc6de1920a
Matthew Leeds <matthew.leeds@endlessm.com>
2019-07-22 18:31:11 -0700

    dir: Ignore trailing slash in remote URIs
    Currently if you have a remote configured with the URL
    "https://dl.flathub.org/repo/" (as you would if you use the flatpakrepo
    file) and you use a flatpakref file which specifies the URL
    "https://dl.flathub.org/repo", Flatpak tries to add a duplicate remote
    because it doesn't see those URLs as equal. So ignore the trailing slash
    when comparing remote URLs. OSTree works equally well with both kinds
    (it uses g_build_filename()).
    
    Flathub served flatpakref files with URLs missing a trailing slash until
    this commit:
    https://github.com/flathub/ansible-playbook/commit/b20694f09
    
    Also, add a unit test that fails without this patch.
    
    Fixes https://github.com/flatpak/flatpak/issues/2979
    
    Closes: #3065
    Approved by: alexlarsson
    
    (cherry picked from commit af4504c8d3a2e00b2d968644517ef8a9180afcd6)

--- tests/test-repo.sh
+++ tests/test-repo.sh
@@ -24,7 +24,7 @@ set -euo pipefail
 skip_without_bwrap
 skip_revokefs_without_fuse
 
-echo "1..32"
+echo "1..33"
 
 #Regular repo
 setup_repo
@@ -177,6 +177,44 @@ fi
 assert_file_has_content install-error-log "Server returned status 404: Not Found"
 
 echo "ok install fails gracefully for 404 URLs"
+
+# Use a new remote so we can be sure it doesn't match any existing one's URL
+setup_repo_no_add flatpakref org.test.Collection.Flatpakref
+
+cat << EOF > repos/flatpakref/flatpakref-repo.flatpakrepo
+[Flatpak Repo]
+Version=1
+Url=http://127.0.0.1:$(cat httpd-port-main)/flatpakref/
+Title=The Title
+GPGKey=${FL_GPG_BASE64}
+EOF
+
+if [ x${USE_COLLECTIONS_IN_CLIENT-} == xyes ]; then
+    echo "DeployCollectionID=org.test.Collection.Flatpakref" >> repos/flatpakref/flatpakref-repo.flatpakrepo
+fi
+
+cat << EOF > org.test.Hello.flatpakref
+[Flatpak Ref]
+Name=org.test.Hello
+Branch=master
+Url=http://127.0.0.1:$(cat httpd-port-main)/flatpakref
+GPGKey=${FL_GPG_BASE64}
+RuntimeRepo=http://127.0.0.1:$(cat httpd-port-main)/flatpakref/flatpakref-repo.flatpakrepo
+EOF
+
+${FLATPAK} ${U} uninstall -y org.test.Platform org.test.Hello
+
+# Ensure that only one remote is added even though the URL in the flatpakref
+# does not have a trailing slash and the URL in the flatpakrepo file does
+NUM_REMOTES_BEFORE=$(flatpak remotes | wc -l)
+${FLATPAK} ${U} install -y org.test.Hello.flatpakref
+NUM_REMOTES_AFTER=$(flatpak remotes | wc -l)
+
+if [ $NUM_REMOTES_AFTER -ne $((NUM_REMOTES_BEFORE + 1)) ]; then
+    assert_not_reached "install of flatpakref should only add one remote"
+fi
+
+echo "ok install flatpakref normalizes remote URL trailing slash"
 
 ${FLATPAK} ${U} uninstall -y org.test.Platform org.test.Hello
 
