62b0d3f4f127edd8c539a7cde1c258ef27409356
Philip Withnall <withnall@endlessm.com>
2019-10-03 11:23:24 +0100

    common: Bump version numbers for new parental controls API
    The PR was written before 1.5.0 was released, but was then merged after
    1.5.0 was released — so bump the version numbers to 1.5.1.
    
    See https://github.com/flatpak/flatpak/pull/2797.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- common/flatpak-error.h
+++ common/flatpak-error.h
@@ -60,7 +60,7 @@ G_BEGIN_DECLS
  * @FLATPAK_ERROR_REF_NOT_FOUND: The specified ref was not found. (Since: 1.4.0)
  * @FLATPAK_ERROR_PERMISSION_DENIED: An operation was not allowed by the administrative policy.
  *                                   For example, an app is not allowed to be installed due
- *                                   to not complying with the parental controls policy. (Since: 1.5.0)
+ *                                   to not complying with the parental controls policy. (Since: 1.5.1)
  *
  * Error codes for library functions.
  */
--- common/flatpak-utils.c
+++ common/flatpak-utils.c
@@ -76,7 +76,7 @@ static const GDBusErrorEntry flatpak_error_entries[] = {
   {FLATPAK_ERROR_WRONG_USER,            "org.freedesktop.Flatpak.Error.WrongUser"}, /* Since: 1.2.0 */
   {FLATPAK_ERROR_NOT_CACHED,            "org.freedesktop.Flatpak.Error.NotCached"}, /* Since: 1.3.3 */
   {FLATPAK_ERROR_REF_NOT_FOUND,         "org.freedesktop.Flatpak.Error.RefNotFound"}, /* Since: 1.4.0 */
-  {FLATPAK_ERROR_PERMISSION_DENIED,     "org.freedesktop.Flatpak.Error.PermissionDenied"}, /* Since: 1.5.0 */
+  {FLATPAK_ERROR_PERMISSION_DENIED,     "org.freedesktop.Flatpak.Error.PermissionDenied"}, /* Since: 1.5.1 */
 };
 
 typedef struct archive FlatpakAutoArchiveRead;
