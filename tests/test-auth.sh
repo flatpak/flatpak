d44864a74eaf598d912687e8739876671cef45e1
Alexander Larsson <alexl@redhat.com>
2019-10-16 11:19:28 +0200

    tests: Add p2p token tests
    Unfortunately we lose some error information when we pull multiple
    refs, ending with a generic "something failed" error rather than the
    401 error so in the p2p case we can't verify that we get the right
    errors.

--- tests/test-auth.sh
+++ tests/test-auth.sh
@@ -39,6 +39,15 @@ mark_need_token () {
     echo -n $TOKEN > repos/$REPO/$(commit_to_obj $COMMIT).need_token
 }
 
+assert_failed_with_401 () {
+    LOGFILE=${1:-install-error-log}
+    # Unfortunately we don't properly return the 401 error in the p2p case...
+    if [ x${USE_COLLECTIONS_IN_CLIENT-} != xyes ] ; then
+        assert_file_has_content $LOGFILE "401"
+    fi
+}
+
+
 # Mark as need token, even though the app doesn't have token-type set
 # We should not be able to install this because we will not present
 # the token unnecessarily
@@ -47,7 +56,7 @@ mark_need_token app/org.test.Hello/$ARCH/master the-secret
 if ${FLATPAK} ${U} install -y test-repo org.test.Hello master 2> install-error-log; then
     assert_not_reached "Should not be able to install with no secret"
 fi
-assert_file_has_content install-error-log "401"
+assert_failed_with_401
 
 # Propertly mark it with token-type
 EXPORT_ARGS="--token-type=2" make_updated_app
@@ -57,7 +66,7 @@ mark_need_token app/org.test.Hello/$ARCH/master the-secret
 if FLATPAK_TEST_TOKEN=not-the-secret ${FLATPAK} ${U} install -y test-repo org.test.Hello master 2> install-error-log; then
     assert_not_reached "Should not be able to install with wrong secret"
 fi
-assert_file_has_content install-error-log "401"
+assert_failed_with_401
 
 # Install with right token
 FLATPAK_TEST_TOKEN=the-secret ${FLATPAK} ${U} install -y test-repo org.test.Hello master
@@ -69,7 +78,7 @@ mark_need_token app/org.test.Hello/$ARCH/master the-secret
 if FLATPAK_TEST_TOKEN=not-the-secret ${FLATPAK} ${U} update -y org.test.Hello 2> install-error-log; then
     assert_not_reached "Should not be able to install with wrong secret"
 fi
-assert_file_has_content install-error-log "401"
+assert_failed_with_401
 
 # Update with right token
 FLATPAK_TEST_TOKEN=the-secret ${FLATPAK} ${U} update -y org.test.Hello
@@ -89,7 +98,7 @@ mark_need_token app/org.test.Hello/$ARCH/copy the-secret
 if FLATPAK_TEST_TOKEN=not-the-secret ${FLATPAK} ${U} install -y test-repo org.test.Hello//copy 2> install-error-log; then
     assert_not_reached "Should not be able to install with wrong secret"
 fi
-assert_file_has_content install-error-log "401"
+assert_failed_with_401
 
 # Install with right token
 FLATPAK_TEST_TOKEN=the-secret ${FLATPAK} ${U} install -y test-repo org.test.Hello//copy
