019885dc87b782bb7f2970a7c71c6ae0dd9b9730
Alexander Larsson <alexl@redhat.com>
2020-01-08 11:09:45 +0100

    Add typename handling

--- variant-parse.py
+++ variant-parse.py
@@ -10,22 +10,39 @@ ident = Word(alphas + "_", alphanums + "_").setName("identifier")
 
 named_types = {}
 
+def add_named_type(name, type):
+    assert not name in named_types
+    type.set_typename(name, True)
+    named_types[name] = type
+
+def get_named_type(name):
+    assert name in named_types
+    return named_types[name]
+
 class TypeDef:
     def __init__(self, name, type):
         self.name = name
         self.type = type
 
-        named_types[name] = type
+        add_named_type(name, type)
 
 class Type:
     def __init__(self):
-        pass
-
-    def resolve(self):
-        return self
+        self.typename = None
 
     def typestring(self):
         assert False
+
+    def set_typename(self, name, override = False):
+        if self.typename == None or override:
+            self.typename = name
+            self.propagate_typename(name)
+
+    def propagate_typename(self, typename):
+        pass
+
+    def is_basic(self):
+        return False
 
 basic_typestrings = {
     "boolean": "b",
@@ -45,45 +62,68 @@ basic_typestrings = {
 
 class BasicType(Type):
     def __init__(self, kind):
+        super().__init__()
         assert kind in basic_typestrings
         self.kind = kind
     def __repr__(self):
          return "BasicType(%s)" % self.kind
     def typestring(self):
          return basic_typestrings[self.kind]
+    def set_typename(self, name):
+        pass # No names for basic types
+    def is_basic(self):
+        return True
 
 class ArrayType(Type):
     def __init__(self, element_type):
+        super().__init__()
         self.element_type = element_type
+
+        if element_type.is_basic():
+            self.typename = self.element_type.kind + "array"
+
     def __repr__(self):
-         return "ArrayType(%s)" % repr(self.element_type)
+         return "ArrayType<%s>(%s)" % (self.typename, repr(self.element_type))
     def typestring(self):
          return "a" + self.element_type.typestring()
+    def propagate_typename(self, name):
+        self.element_type.set_typename (name + "__element")
 
 class DictType(Type):
     def __init__(self, key_type, element_type):
+        super().__init__()
         self.key_type = key_type
         self.element_type = element_type
     def __repr__(self):
-         return "DictType(%s, %s)" % (repr(self.key_type), repr(self.element_type))
+         return "DictType<%s>(%s, %s)" % (self.typename, repr(self.key_type), repr(self.element_type))
     def typestring(self):
          return "a{%s%s}" % (self.key_type.typestring(), self.element_type.typestring())
+    def propagate_typename(self, name):
+        self.element_type.set_typename (name + "__element")
 
 class MaybeType(Type):
     def __init__(self, element_type):
+        super().__init__()
         self.element_type = element_type
+        if element_type.is_basic():
+            self.typename = "maybe" + self.element_type.kind
     def __repr__(self):
-         return "MaybeType(%s)" % repr(self.element_type)
+         return "MaybeType<%s>(%s, %s)" % (self.typename, repr(self.element_type))
     def typestring(self):
          return "m" + self.element_type.typestring()
+    def propagate_typename(self, name):
+        self.element_type.set_typename (name + "__element")
 
 class VariantType(Type):
     def __init__(self):
-        pass
+        super().__init__()
+        self.typename = "variant"
     def __repr__(self):
          return "VariantType()"
     def typestring(self):
          return "v"
+    def set_typename(self, name):
+        pass # No names for variant
 
 class Field:
     def __init__(self, name, attributes, type):
@@ -94,11 +134,15 @@ class Field:
     def __repr__(self):
          return "Field(%s, %s)" % (self.name, self.type)
 
+    def propagate_typename(self, struct_name):
+        self.type.set_typename (struct_name + "__" + self.name)
+
 class StructType(Type):
     def __init__(self, fields):
+        super().__init__()
         self.fields = list(fields)
     def __repr__(self):
-        return "StructType(%s)" % ",".join(map(repr, self.fields))
+        return "StructType<%s>(%s)" % (self.typename, ",".join(map(repr, self.fields)))
 
     def typestring(self):
         res = ['(']
@@ -107,18 +151,9 @@ class StructType(Type):
         res.append(')')
         return "".join(res)
 
-class NamedType(Type):
-    def __init__(self, name):
-        self.name = name
-    def __repr__(self):
-        return "NamedType(%s)" % self.name
-
-    def resolve(self):
-        assert self.name in named_types
-        return named_types[self.name]
-
-    def typestring(self):
-        return self.resolve().typestring()
+    def propagate_typename(self, name):
+        for f in self.fields:
+            f.propagate_typename(name)
 
 typeSpec = Forward()
 
@@ -138,7 +173,7 @@ field = (ident + COLON + Group(ZeroOrMore(fieldAttribute)) + typeSpec + SEMI).se
 
 structType = (LBRACE + ZeroOrMore(field) + RBRACE).setParseAction(lambda toks: StructType(toks))
 
-namedType = ident.copy().setParseAction(lambda toks: NamedType(toks[0]))
+namedType = ident.copy().setParseAction(lambda toks: get_named_type(str(toks[0])))
 
 typeSpec <<= basicType ^ arrayType ^ maybeType ^ variantType ^ dictType ^ structType ^ namedType
 
@@ -153,5 +188,6 @@ if __name__ == "__main__":
         results = typeDefs.parseString(testdata, parseAll=True)
         for t in results:
             print("%s - %s" % (t.name, t.type.typestring()))
+            print("> ", t.type)
     except ParseException as pe:
         print("Parse error:", pe)
