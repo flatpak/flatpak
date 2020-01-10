12845735f54d818c424f8b5052a9f8f457849ded
Alexander Larsson <alexl@redhat.com>
2020-01-10 13:23:39 +0100

    Add more testing to sample.c

--- sample.c
+++ sample.c
@@ -1,52 +1,71 @@
 #include "sample.h"
 
+void
+test_sample_variant (GVariant *v)
+{
+  Test t;
+  Var var;
+  GVariant *varv;
+  const char *res;
+  variant resv;
+
+  g_print ("sample: %s\n", g_variant_print (v, FALSE));
+  g_print ("sample with types: %s\n", g_variant_print (v, TRUE));
+
+  g_assert (g_variant_type_equal (g_variant_get_type(v), Test_typeformat));
+
+  t = Test_from_gvariant (v);
+  g_print ("custom: %s\n", Test_print (t, FALSE));
+  g_print ("custom with types: %s\n", Test_print (t, TRUE));
+
+  g_assert_cmpstr (g_variant_print (v, FALSE), ==, Test_print (t, FALSE));
+  g_assert_cmpstr (g_variant_print (v, TRUE), ==, Test_print (t, TRUE));
+
+  var = Var_from_variant(Test_get_v(t));
+  varv = g_variant_get_variant (g_variant_get_child_value (v, Test_indexof_v));
+  g_assert_cmpstr (g_variant_print (varv, TRUE), ==, Var_print (var, TRUE));
+
+  Test__d2 d2 = Test_get_d2(t);
+  GVariant *v3 = Test__d2_dup_to_gvariant(d2);
+  g_assert_cmpstr (g_variant_print (v3, TRUE), ==, Test__d2_print(d2, FALSE));
+
+  g_assert (Test__d2_lookup(d2, 1, &res));
+  g_assert_cmpstr (res, ==, "a");
+  g_assert (Test__d2_lookup(d2, 3, &res));
+  g_assert_cmpstr (res, ==, "b");
+  g_assert (!Test__d2_lookup(d2, 2, &res));
+
+  Metadata meta = Test_get_meta(t);
+  g_print ("meta: %s\n", Metadata_print(meta, FALSE));
+  GVariant *meta_v = Metadata_dup_to_gvariant(meta);
+
+  g_assert (g_variant_type_equal (g_variant_get_type(meta_v), Metadata_typeformat));
+  g_assert_cmpstr (g_variant_print (meta_v, FALSE), ==, Metadata_print (meta, FALSE));
+  g_assert_cmpstr (g_variant_print (meta_v, TRUE), ==, Metadata_print (meta, TRUE));
+
+  g_assert (Metadata_lookup(meta, "foo", &resv));
+  g_assert_cmpstr ("<1>", ==, variant_print (resv, TRUE));
+  g_assert (Metadata_lookup(meta, "bar", &resv));
+  g_assert_cmpstr ("<'s'>", ==, variant_print (resv, TRUE));
+  g_assert (!Metadata_lookup(meta, "missing", &resv));
+}
+
 int
 main (int argc,
       char *argv[])
 {
   GVariant *v;
-  Test t;
-  Var vv;
-  const char *res;
-  variant resv;
 
-  v = g_variant_new_parsed ("([32, 22], 's', uint16 16, ('s2', 322), (323,), 324, <(int16 67, 1023)>, [(int16 68, 1025), (int16 69, 1026)]"
-                            ", {1:2, 3:4}, {'foo': <1>, 'bar': <'s'>}, {1:'a', 3:'b'}"
-                            ")");
-  g_print ("v: %s\n", g_variant_print (v, FALSE));
-  g_print ("v2: %s\n", g_variant_print (v, TRUE));
+#define DATA \
+  "([32, 22], '%s', uint16 16, "                                   \
+    "('s2', 322), (323,), 324, <(int16 67, 1023)>, "              \
+    "[(int16 68, 1025), (int16 69, 1026)]"                        \
+    ", {1:2, 3:4}, {'foo': <1>, 'bar': <'s'>}, {1:'a', 3:'b'}"    \
+    ")"
 
-  g_assert (g_variant_type_equal (g_variant_get_type(v), Test_typeformat));
+  v = g_variant_new_parsed (g_strdup_printf (DATA, "s"));
 
-  t = Test_from_gvariant (v);
-  g_print ("t: %s\n", Test_print (t, FALSE));
-  g_print ("t2: %s\n", Test_print (t, TRUE));
-  //TODO: g_assert_cmpstr (g_variant_print (v, FALSE), ==, Test_print (t, FALSE));
-
-  vv = Var_from_variant(Test_get_v(t));
-  g_print ("t.v: %s\n", Var_print (vv, TRUE));
-
-  Test__d2 d2 = Test_get_d2(t);
-  g_print ("t.d2: %s\n", Test__d2_print(d2, FALSE));
-  GVariant *v3 = Test__d2_dup_to_gvariant(d2);
-  g_print ("t.d2 as gvariant: %s\n", g_variant_print (v3, TRUE));
-
-  if (Test__d2_lookup(d2, 1, &res))
-    g_print ("t.d2.lookup(1): %s\n", res);
-  if (Test__d2_lookup(d2, 3, &res))
-    g_print ("t.d2.lookup(3): %s\n", res);
-  if (Test__d2_lookup(d2, 2, &res))
-    g_print ("t.d2.lookup(2): didn't fail\n");
-
-  Metadata meta = Test_get_meta(t);
-  g_print ("meta: %s\n", Metadata_print(meta, FALSE));
-  GVariant *meta_v = Metadata_dup_to_gvariant(meta);
-  g_print ("meta as gvariant: %s\n", g_variant_print (meta_v, TRUE));
-
-  if (Metadata_lookup(meta, "foo", &resv))
-    g_print ("t.meta[\"foo\"]=%s\n", variant_print (resv, TRUE));
-  if (Metadata_lookup(meta, "bar", &resv))
-    g_print ("t.meta[\"bar\"]=%s\n", variant_print (resv, TRUE));
-  if (Metadata_lookup(meta, "missing", &resv))
-    g_print ("t.meta[\"missing\"] didn't fail!\n");
+  /* Try with larger offsets */
+  v = g_variant_new_parsed (g_strdup_printf (DATA, "sxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
+  test_sample_variant(v);
 }
