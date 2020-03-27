flatpak/flatpak

commit b03916f5bdf878ba42af7ccf569233b839d4b3d2

setup-extra-data: Avoid extra work for ostree-metadata and appstream branches
We never have extra data for non app/ or runtime/ refs, so lets not
do an unnecessary pull there.

common/flatpak-dir.c
@@ -4941,39 +4941,43 @@ flatpak_dir_setup_extra_data (FlatpakDir                           *self,
   /* If @results is set, @rev must be. */
   g_assert (results == NULL || rev != NULL);
 
-  extra_data_sources = flatpak_repo_get_extra_data_sources (repo, rev, cancellable, NULL);
-  if (extra_data_sources == NULL)
+  /* ostree-metadata and appstreams never have extra data, so ignore those */
+  if (g_str_has_prefix ("app/", ref) || g_str_has_prefix ("runtime/", ref))
     {
-      /* This is a gigantic hack where we download the commit in a temporary transaction
-       * which we then abort after having read the result. We do this to avoid creating
-       * a partial commit in the local repo and a ref that points to it, because that
-       * causes ostree to not use static deltas.
-       * See https://github.com/flatpak/flatpak/issues/3412 for details.
-       */
-
-      if (!ostree_repo_prepare_transaction (repo, NULL, cancellable, error))
-        return FALSE;
-
-      /* Pull the commits (and only the commits) to check for extra data
-       * again. Here we don't pass the progress because we don't want any
-       * reports coming out of it. */
-      if (!repo_pull (repo, repository,
-                      NULL,
-                      ref,
-                      rev,
-                      token,
-                      results,
-                      flatpak_flags,
-                      OSTREE_REPO_PULL_FLAGS_COMMIT_ONLY,
-                      NULL,
-                      cancellable,
-                      error))
-        return FALSE;
-
       extra_data_sources = flatpak_repo_get_extra_data_sources (repo, rev, cancellable, NULL);
+      if (extra_data_sources == NULL)
+        {
+          /* This is a gigantic hack where we download the commit in a temporary transaction
+           * which we then abort after having read the result. We do this to avoid creating
+           * a partial commit in the local repo and a ref that points to it, because that
+           * causes ostree to not use static deltas.
+           * See https://github.com/flatpak/flatpak/issues/3412 for details.
+           */
 
-      if (!ostree_repo_abort_transaction (repo, cancellable, error))
-        return FALSE;
+          if (!ostree_repo_prepare_transaction (repo, NULL, cancellable, error))
+            return FALSE;
+
+          /* Pull the commits (and only the commits) to check for extra data
+           * again. Here we don't pass the progress because we don't want any
+           * reports coming out of it. */
+          if (!repo_pull (repo, repository,
+                          NULL,
+                          ref,
+                          rev,
+                          token,
+                          results,
+                          flatpak_flags,
+                          OSTREE_REPO_PULL_FLAGS_COMMIT_ONLY,
+                          NULL,
+                          cancellable,
+                          error))
+            return FALSE;
+
+          extra_data_sources = flatpak_repo_get_extra_data_sources (repo, rev, cancellable, NULL);
+
+          if (!ostree_repo_abort_transaction (repo, cancellable, error))
+            return FALSE;
+        }
     }
 
   n_extra_data = 0;

commit ed3ba39a06d47d384a50e3a6264d411f3bf20a5d

Fix calculation of extra-data total size
This is a bug introduced in b03916f5bdf878ba42af7ccf569233b839d4b3d2
where we check the extra_data refs against app/ or runtime/ prefix with
arguments in the wrong order.

common/flatpak-dir.c
@@ -4608,9 +4608,8 @@ flatpak_dir_setup_extra_data (FlatpakDir                           *self,
   guint64 n_extra_data;
   guint64 total_download_size;
 
-
   /* ostree-metadata and appstreams never have extra data, so ignore those */
-  if (g_str_has_prefix ("app/", ref) || g_str_has_prefix ("runtime/", ref))
+  if (g_str_has_prefix (ref, "app/") || g_str_has_prefix (ref, "runtime/"))
     {
       extra_data_sources = flatpak_repo_get_extra_data_sources (repo, rev, cancellable, NULL);
       if (extra_data_sources == NULL)