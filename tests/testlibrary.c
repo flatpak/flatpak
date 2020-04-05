cb726a7569452dda12a5f8a50c97ebd8cef5f902
Matthew Leeds <matthew.leeds@endlessm.com>
2020-04-05 10:39:12 -0700

    testlibrary: Fix a whitespace issue

--- tests/testlibrary.c
+++ tests/testlibrary.c
@@ -1185,7 +1185,7 @@ test_update_related_refs (void)
                                        FLATPAK_REF_KIND_APP,
                                        "org.test.Hello",
                                        NULL, "master", NULL, NULL, NULL,
-				                               &error);
+                                       &error);
   G_GNUC_END_IGNORE_DEPRECATIONS
   g_assert_no_error (error);
   g_assert (FLATPAK_IS_INSTALLED_REF (iref));
