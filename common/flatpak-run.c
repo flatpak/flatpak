f3c0d3e87ff75734d82cf9a40b18735893770345
Jan Grulich <jgrulich@redhat.com>
2020-04-15 13:36:18 +0200

    CUPS socket: do not print error when we found CUPS server

--- common/flatpak-run.c
+++ common/flatpak-run.c
@@ -420,9 +420,10 @@ flatpak_run_add_cups_args (FlatpakBwrap *bwrap)
   g_autofree char * cups_server_name = flatpak_run_get_cups_server_name();
 
   if (!g_file_test (cups_server_name, G_FILE_TEST_EXISTS))
-    return;
-  else
-    g_debug ("Could not find CUPS server");
+    {
+      g_debug ("Could not find CUPS server");
+      return;
+    }
 
   flatpak_bwrap_add_args (bwrap,
                           "--ro-bind", cups_server_name, sandbox_server_name,
