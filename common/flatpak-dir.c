aaa36bab45f8a1bd22d10b04a470fcfdd3dc460b
Alexander Larsson <alexl@redhat.com>
2020-05-08 15:15:50 +0200

    oci: Support deltas when using system-helper
    When mirroring to a local OCI dir we apply deltas and generate
    uncompressed layer blobs instead of regular blobs. Then we pick this
    up on the system-helper side.

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -5145,7 +5145,7 @@ flatpak_dir_mirror_oci (FlatpakDir          *self,
 
   g_debug ("Mirroring OCI image %s", oci_digest);
 
-  res = flatpak_mirror_image_from_oci (dst_registry, registry, oci_repository, oci_digest, ref, oci_pull_progress_cb,
+  res = flatpak_mirror_image_from_oci (dst_registry, registry, oci_repository, oci_digest, state->remote_name, ref, self->repo, oci_pull_progress_cb,
                                        progress, cancellable, error);
 
   if (!res)
