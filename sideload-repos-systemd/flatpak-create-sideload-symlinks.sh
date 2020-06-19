9caf664fabc911419cc483125bf543c4550604c7
Matthew Leeds <matthew.leeds@endlessm.com>
2020-06-18 20:56:47 -0700

    Add systemd units to sideload from hotplugged USBs
    Currently with the sideload implementation of offline updates you have
    to manually create a symlink to your USB drive to sideload from it,
    which is a regression compared to the previous implementation which
    scanned all mounted filesystems in OstreeRepoFinderMount in libostree.
    So this commit adds a few systemd units and a bash script so that any
    time a USB drive is plugged in and automatically mounted by udisks, a
    symlink to it is created in /run/flatpak/sideload-repos. When the drive
    is unplugged the symlink is removed.
    
    However this solution still has a lot of moving parts, so we may want to
    instead have libflatpak use GVolumeMonitor and find the mounted
    filesystems itself; see https://github.com/flatpak/flatpak/issues/3705
    
    Fixes https://github.com/flatpak/flatpak/issues/3490

---
+++ sideload-repos-systemd/flatpak-create-sideload-symlinks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script is intended to be run by flatpak-sideload-usb-repo.service
+
+if ! test $# -eq 1 || ! test -d "$1"; then
+  echo "Error: first argument must be a directory"
+  exit 1
+fi
+
+# Add a link to any newly inserted drives which might have been copied to with
+# "flatpak create-usb". If we were to check for a repo on the drive that would
+# break the case of using it for sideloading directly after copying to it (e.g.
+# for testing).
+for f in "$1"/*; do
+    if ! test -d "$f"; then
+        continue
+    fi
+    unique_name=automount-$(echo "$f" | sha256sum | cut -f 1 -d ' ')
+    if test -e "/run/flatpak/sideload-repos/$unique_name"; then
+        continue
+    fi
+    ln -s "$f" "/run/flatpak/sideload-repos/$unique_name"
+done
+
+# Remove any broken symlinks e.g. from drives that were removed
+for f in /run/flatpak/sideload-repos/automount-*; do
+    if ! test -e "$f"; then
+        rm "$f"
+    fi
+done
