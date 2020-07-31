flatpak/flatpak

commit 08d65c5414d756c957549e14dcc599a9ed5a2a91

exports: If --filesystem=host, provide /run/host/lib etc.
In a host system where the /usr merge has not been implemented, these can
be necessary to load or inspect libraries or executables from the host
system. They are conceptually the same as /usr.

Signed-off-by: Simon McVittie <smcv@collabora.com>

common/flatpak-exports.c
@@ -282,6 +282,7 @@ flatpak_exports_append_bwrap_args (FlatpakExports *exports,
   if (exports->host_fs != 0)
     {
       const char *host_bind_mode = "--bind";
+      int i;
 
       if (exports->host_fs == FLATPAK_FILESYSTEM_MODE_READ_ONLY)
         host_bind_mode = "--ro-bind";
@@ -289,6 +290,50 @@ flatpak_exports_append_bwrap_args (FlatpakExports *exports,
       if (g_file_test ("/usr", G_FILE_TEST_IS_DIR))
         flatpak_bwrap_add_args (bwrap,
                                 host_bind_mode, "/usr", "/run/host/usr", NULL);
+
+      for (i = 0; flatpak_abs_usrmerged_dirs[i] != NULL; i++)
+        {
+          const char *subdir = flatpak_abs_usrmerged_dirs[i];
+          g_autofree char *target = NULL;
+          g_autofree char *run_host_subdir = NULL;
+
+          g_assert (subdir[0] == '/');
+          /* e.g. /run/host/lib32 */
+          run_host_subdir = g_strconcat ("/run/host", subdir, NULL);
+          target = glnx_readlinkat_malloc (-1, subdir, NULL, NULL);
+
+          if (target != NULL &&
+              g_str_has_prefix (target, "usr/") &&
+              g_strcmp0 (target + 3, subdir) == 0)
+            {
+              /* e.g. /lib32 is a relative symlink to usr/lib32;
+               * keep it relative */
+              flatpak_bwrap_add_args (bwrap,
+                                      "--symlink", target, run_host_subdir,
+                                      NULL);
+            }
+          else if (target != NULL &&
+                   g_str_has_prefix (target, "/usr/") &&
+                   g_strcmp0 (target + 4, subdir) == 0)
+            {
+              /* e.g. /lib32 is an absolute symlink to /usr/lib32; make
+               * it a relative symlink to usr/lib32 instead by skipping
+               * the '/' */
+              flatpak_bwrap_add_args (bwrap,
+                                      "--symlink", target + 1, run_host_subdir,
+                                      NULL);
+            }
+          else if (g_file_test (subdir, G_FILE_TEST_IS_DIR))
+            {
+              /* e.g. /lib32 is a symlink to /opt/compat/ia32/lib,
+               * or is a plain directory because the host OS has not
+               * undergone the /usr merge; bind-mount the directory instead */
+              flatpak_bwrap_add_args (bwrap,
+                                      host_bind_mode, subdir, run_host_subdir,
+                                      NULL);
+            }
+        }
+
       if (g_file_test ("/etc", G_FILE_TEST_IS_DIR))
         flatpak_bwrap_add_args (bwrap,
                                 host_bind_mode, "/etc", "/run/host/etc", NULL);

commit d1638bbfb80b3a4085da7ec6a19a3e49ad68dc46

exports: Try harder to export usrmerge symlinks as symlinks
In applications with --filesystem=host or --filesystem=host-os, the
library directories in /run/host/usr and /run/host/lib* can be used
like a sysroot to inspect the host's library stack, regardless of
whether the host system has undergone the "/usr merge" or not. This is
particularly relevant for Steam's pressure-vessel container tool,
which imports graphics drivers from the host system, or for potential
future work on using host graphics drivers via libcapsule in Flatpak
apps.

The original implementation of this feature assumed that the /usr merge
always creates symbolic links /foo -> /usr/foo or /foo -> usr/foo, for
some value of foo. However, Arch Linux uses a variation of multilib
in which /usr/lib contains 64-bit libraries, and has a non-matching
symbolic link /lib64 -> usr/lib (instead of a chain of symlinks
/lib64 -> usr/lib64 -> lib). Similarly, Arch uses /sbin -> usr/bin
and /usr/sbin -> bin, instead of a chain of symlinks
/sbin -> usr/sbin -> bin.

This led to Flatpak ignoring the symlink and treating it like a directory,
so the host /usr/lib would be bind-mounted on both /run/host/usr/lib and
/run/host/lib64. This is *mostly* equivalent, but whether a directory
is a symlink or a real directory sometimes matters, for example when
evaluating relative symlinks that contain ".."  components; so try harder
to preserve symlinks as symlinks.

Fixes: 08d65c54 "exports: If --filesystem=host, provide /run/host/lib etc."
Signed-off-by: Simon McVittie <smcv@collabora.com>

common/flatpak-exports.c
@@ -325,18 +325,17 @@ flatpak_exports_append_bwrap_args (FlatpakExports *exports,
           target = glnx_readlinkat_malloc (-1, subdir, NULL, NULL);
 
           if (target != NULL &&
-              g_str_has_prefix (target, "usr/") &&
-              g_strcmp0 (target + 3, subdir) == 0)
+              g_str_has_prefix (target, "usr/"))
             {
-              /* e.g. /lib32 is a relative symlink to usr/lib32;
+              /* e.g. /lib32 is a relative symlink to usr/lib32, or
+               * on Arch Linux, /lib64 is a relative symlink to usr/lib;
                * keep it relative */
               flatpak_bwrap_add_args (bwrap,
                                       "--symlink", target, run_host_subdir,
                                       NULL);
             }
           else if (target != NULL &&
-                   g_str_has_prefix (target, "/usr/") &&
-                   g_strcmp0 (target + 4, subdir) == 0)
+                   g_str_has_prefix (target, "/usr/"))
             {
               /* e.g. /lib32 is an absolute symlink to /usr/lib32; make
                * it a relative symlink to usr/lib32 instead by skipping