69a0c6f7010936e4b83b4f734a1dd9855589ab2a
Alexander Larsson <alexl@redhat.com>
2020-11-09 14:48:59 +0100

    dir: Don't crash in get_summary_for_ref() if arch has not non-compat arch

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -583,16 +583,21 @@ static GVariant *
 get_summary_for_ref (FlatpakRemoteState *self,
                      const char *ref)
 {
-  GVariant *summary;
+  GVariant *summary = NULL;
+
   if (self->index != NULL)
     {
       g_autofree char * arch = flatpak_get_arch_for_ref (ref);
 
-      summary = g_hash_table_lookup (self->subsummaries, arch);
+      if (arch != NULL)
+        summary = g_hash_table_lookup (self->subsummaries, arch);
+
       if (summary == NULL)
         {
           const char *non_compat_arch = flatpak_get_compat_arch_reverse (arch);
-          summary = g_hash_table_lookup (self->subsummaries, non_compat_arch);
+
+          if (non_compat_arch != NULL)
+            summary = g_hash_table_lookup (self->subsummaries, non_compat_arch);
         }
     }
   else
