5e0604657a8ad578b3bac0f6e524e395c5194ce5
Simon McVittie <smcv@collabora.com>
2021-01-11 12:48:01 +0000

    tests: Assert that --env= does not go in `flatpak run` or bwrap environ
    For the portal's use of --env-fd= to be safe, we want the environment
    variables that it sets to end up in the environment for the program
    that is run by `bwrap` as process 2, but they must not go into the
    environment that gets used to run `flatpak run` or `bwrap`. Assert
    that this is the case.
    
    For completeness, we're testing both --env= and --env-fd= here,
    even though the earlier commit
    "portal: Do not use caller-supplied variables in environment"
    always uses --env-fd=.
    
    Part-of: https://github.com/flatpak/flatpak/security/advisories/GHSA-4ppf-fxf6-vxg2
    Signed-off-by: Simon McVittie <smcv@collabora.com>
    (cherry picked from commit 5019b303f66de90ec773a4ea95114b0e47578f52)

---
+++ tests/libpreload.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Collabora Ltd.
+ * SPDX-License-Identifier: LGPL-2-or-later
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+__attribute__((constructor)) static void
+ctor (void)
+{
+  pid_t me = getpid ();
+  struct stat buf;
+
+  fprintf (stderr, "LD_PRELOAD module got loaded by process %d\n", me);
+
+  if (stat ("/.flatpak-info", &buf) == 0)
+    {
+      fprintf (stderr, "OK: pid %d is in a Flatpak sandbox\n", me);
+    }
+  else
+    {
+      /* If the --env=LD_PRELOAD had come from a call to flatpak-portal,
+       * then this would be a sandbox escape (GHSA-4ppf-fxf6-vxg2). */
+      fprintf (stderr, "Error: pid %d is not in a Flatpak sandbox\n", me);
+      abort ();
+    }
+}
