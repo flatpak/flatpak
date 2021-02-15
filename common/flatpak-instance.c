bc595e30ab91150ba93650e42b250baf23d2523c
Simon McVittie <smcv@collabora.com>
2021-02-15 12:58:02 +0000

    common: Move flatpak_run_allocate_id() to flatpak-instance
    This localizes knowledge of the internal structure of
    $XDG_RUNTIME_DIR/.flatpak into the flatpak-instance module.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- common/flatpak-instance.c
+++ common/flatpak-instance.c
@@ -420,6 +420,71 @@ flatpak_instance_new (const char *dir)
   return self;
 }
 
+/*
+ * @host_dir_out: (not optional): used to return the directory on the host
+ *  system representing this instance
+ * @lock_fd_out: (not optional): used to return a non-exclusive (read) lock
+ *  on the lockdirectory on the host-file
+ */
+char *
+flatpak_instance_allocate_id (char **host_dir_out,
+                              int *lock_fd_out)
+{
+  g_autofree char *user_runtime_dir = flatpak_get_real_xdg_runtime_dir ();
+  g_autofree char *base_dir = g_build_filename (user_runtime_dir, ".flatpak", NULL);
+  int count;
+
+  g_return_val_if_fail (host_dir_out != NULL, NULL);
+  g_return_val_if_fail (*host_dir_out == NULL, NULL);
+  g_return_val_if_fail (lock_fd_out != NULL, NULL);
+  g_return_val_if_fail (*lock_fd_out == -1, NULL);
+
+  g_mkdir_with_parents (base_dir, 0755);
+
+  flatpak_instance_iterate_all_and_gc (NULL);
+
+  for (count = 0; count < 1000; count++)
+    {
+      g_autofree char *instance_id = NULL;
+      g_autofree char *instance_dir = NULL;
+
+      instance_id = g_strdup_printf ("%u", g_random_int ());
+
+      instance_dir = g_build_filename (base_dir, instance_id, NULL);
+
+      /* We use an atomic mkdir to ensure the instance id is unique */
+      if (mkdir (instance_dir, 0755) == 0)
+        {
+          g_autofree char *lock_file = g_build_filename (instance_dir, ".ref", NULL);
+          glnx_autofd int lock_fd = -1;
+          struct flock l = {
+            .l_type = F_RDLCK,
+            .l_whence = SEEK_SET,
+            .l_start = 0,
+            .l_len = 0
+          };
+
+          /* Then we take a file lock inside the dir, hold that during
+           * setup and in bwrap. Anyone trying to clean up unused
+           * directories need to first verify that there is a .ref
+           * file and take a write lock on .ref to ensure its not in
+           * use. */
+          lock_fd = open (lock_file, O_RDWR | O_CREAT | O_CLOEXEC, 0644);
+          /* There is a tiny race here between the open creating the file and the lock succeeding.
+             We work around that by only gc:ing "old" .ref files */
+          if (lock_fd != -1 && fcntl (lock_fd, F_SETLK, &l) == 0)
+            {
+              *lock_fd_out = glnx_steal_fd (&lock_fd);
+              g_debug ("Allocated instance id %s", instance_id);
+              *host_dir_out = g_steal_pointer (&instance_dir);
+              return g_steal_pointer (&instance_id);
+            }
+        }
+    }
+
+  return NULL;
+}
+
 FlatpakInstance *
 flatpak_instance_new_for_id (const char *id)
 {
@@ -462,7 +527,8 @@ flatpak_instance_iterate_all_and_gc (GPtrArray *out_instances)
           glnx_autofd int lock_fd = openat (iter.fd, ref_file, O_RDWR | O_CLOEXEC);
           if (lock_fd != -1 &&
               fstat (lock_fd, &statbuf) == 0 &&
-              /* Only gc if created at least 3 secs ago, to work around race mentioned in flatpak_run_allocate_id() */
+              /* Only gc if created at least 3 secs ago, to work around race mentioned in
+               * flatpak_instance_allocate_id() */
               statbuf.st_mtime + 3 < time (NULL) &&
               fcntl (lock_fd, F_GETLK, &l) == 0 &&
               l.l_type == F_UNLCK)
