19a93a84270fb5d117646415e5f20a320a5cad42
Simon McVittie <smcv@collabora.com>
2021-10-21 18:43:50 +0100

    run: Always create .local/state directory
    This is placed at the same location that would be used by
    the --persist=.local/state option.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- common/flatpak-run.c
+++ common/flatpak-run.c
@@ -1904,6 +1904,7 @@ flatpak_ensure_data_dir (GFile        *app_id_dir,
   g_autoptr(GFile) fontconfig_cache_dir = g_file_get_child (cache_dir, "fontconfig");
   g_autoptr(GFile) tmp_dir = g_file_get_child (cache_dir, "tmp");
   g_autoptr(GFile) config_dir = g_file_get_child (app_id_dir, "config");
+  g_autoptr(GFile) state_dir = g_file_get_child (app_id_dir, ".local/state");
 
   if (!flatpak_mkdir_p (data_dir, cancellable, error))
     return FALSE;
@@ -1918,6 +1919,9 @@ flatpak_ensure_data_dir (GFile        *app_id_dir,
     return FALSE;
 
   if (!flatpak_mkdir_p (config_dir, cancellable, error))
+    return FALSE;
+
+  if (!flatpak_mkdir_p (state_dir, cancellable, error))
     return FALSE;
 
   return TRUE;
