d37c739f6cdda3d269ea7c7b630c18fc154bb971
Ryan Gonzalez <ryan.gonzalez@collabora.com>
2021-11-13 17:17:14 -0600

    Add a profile script for Fish
    Technically, Fish users can already make this work using
    https://github.com/edc/bass, but it's also nice to have this working on
    an out-of-the-box Flatpak installation.
    
    Fixes #3109.
    
    Signed-off-by: Ryan Gonzalez <ryan.gonzalez@collabora.com>

---
+++ profile/flatpak.fish
@@ -0,0 +1,17 @@
+if type -q flatpak
+    # Set XDG_DATA_DIRS to include Flatpak installations
+
+    set -x --path XDG_DATA_DIRS
+
+    set -q XDG_DATA_DIRS[1]; or set XDG_DATA_DIRS /usr/local/share /usr/share
+    set -q XDG_DATA_HOME; or set -l XDG_DATA_HOME $HOME/.local/share
+
+    set -l installations $XDG_DATA_HOME/flatpak
+    begin
+        set -le G_MESSAGES_DEBUG
+        set -lx GIO_USE_VFS local
+        set installations $installations (flatpak --installations)
+    end
+
+    set XDG_DATA_DIRS {$installations}/exports/share $XDG_DATA_DIRS
+end
