flatpak/flatpak

commit 701602c6cab1a1c587e7512217175f8826dae94a

system-helper: Use modify-repo action for metadata updates
Currently by an accident of history when the system-helper is asked to
deploy updates to the repo metadata (stored on the ref
"ostree-metadata") it uses the polkit action
org.freedesktop.Flatpak.runtime-install since the ref doesn't start with
"app/" and is therefore assumed to be a runtime. This of course doesn't
make much sense, so this commit redirects such invocations of the
"Deploy" method to the "modify-repo" action, which is a bit of a
catch-all of things the system-helper should be allowed to do. It
doesn't seem necessary to split this out into its own action, since
sysadmins probably don't need the ability to break Flatpak's expected
functionality by disabling it. See the PR for more discussion.

Fixes https://github.com/flatpak/flatpak/issues/2328

Closes: #2351
Approved by: matthiasclasen

system-helper/flatpak-system-helper.c
@@ -1225,22 +1225,31 @@ flatpak_authorize_method_handler (GDBusInterfaceSkeleton *interface,
       g_variant_get_child (parameters, 2, "&s", &ref);
       g_variant_get_child (parameters, 3, "&s", &origin);
 
-      is_update = (flags & FLATPAK_HELPER_DEPLOY_FLAGS_UPDATE) != 0;
-      is_app = g_str_has_prefix (ref, "app/");
-
-      if (is_update)
+      /* For metadata updates, redirect to the modify-repo action since they
+       * should basically always be allowed */
+      if (ref != NULL && g_strcmp0 (ref, OSTREE_REPO_METADATA_REF) == 0)
         {
-          if (is_app)
-            action = "org.freedesktop.Flatpak.app-update";
-          else
-            action = "org.freedesktop.Flatpak.runtime-update";
+          action = "org.freedesktop.Flatpak.modify-repo";
         }
       else
         {
-          if (is_app)
-            action = "org.freedesktop.Flatpak.app-install";
+          is_update = (flags & FLATPAK_HELPER_DEPLOY_FLAGS_UPDATE) != 0;
+          is_app = g_str_has_prefix (ref, "app/");
+
+          if (is_update)
+            {
+              if (is_app)
+                action = "org.freedesktop.Flatpak.app-update";
+              else
+                action = "org.freedesktop.Flatpak.runtime-update";
+            }
           else
-            action = "org.freedesktop.Flatpak.runtime-install";
+            {
+              if (is_app)
+                action = "org.freedesktop.Flatpak.app-install";
+              else
+                action = "org.freedesktop.Flatpak.runtime-install";
+            }
         }
 
       polkit_details_insert (details, "origin", origin);

commit 47a265dcd1b0dab89099ab3f5aaac3b44194a7a5

system-helper: Remove redundant (and misleading) NULL check
First of all, g_strcmp0() is NULL-safe, so there's no need to check if
ref_str is NULL or not.

Secondly, the NULL check triggers Coverity.  It thinks that ref_str
might be NULL in the 'else' branch, where ref_str is unconditionally
accessed by flatpak_decomposed_new_from_ref().  There's no need to be
concerned about this because ref_str can't be NULL.  It's one of the
parameters of the o.fd.Flatpak.SystemHelper.Deploy D-Bus method.  It's
defined as an 's', and NULL is not a valid value for 's'.  In fact,
this D-Bus argument is assumed to be not NULL when the Deploy method is
handled.

Fallout from 701602c6cab1a1c587e7512217175f8826dae94a

system-helper/flatpak-system-helper.c
@@ -1934,7 +1934,7 @@ flatpak_authorize_method_handler (GDBusInterfaceSkeleton *interface,
 
       /* For metadata updates, redirect to the metadata-update action which
        * should basically always be allowed */
-      if (ref_str != NULL && g_strcmp0 (ref_str, OSTREE_REPO_METADATA_REF) == 0)
+      if (g_strcmp0 (ref_str, OSTREE_REPO_METADATA_REF) == 0)
         {
           action = "org.freedesktop.Flatpak.metadata-update";
         }