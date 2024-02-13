flatpak/flatpak

commit 88a928ea6241800e48cb722070efca6c5a1bbb89

run: Avoid execve() when measuring test coverage
Normally, we want to save a process and get better signal handling
by replacing the `flatpak run` process with bubblewrap.

However, when we're doing profiling or measuring coverage, we want to
exit cleanly so that profiling data can be recorded, which is done in
an atexit() hook. In this situation, bypass the execve() optimization;
instead, start bubblewrap in the background, immediately wait for it,
and propagate its exit status.

Signed-off-by: Simon McVittie <smcv@collabora.com>

common/flatpak-run.c
@@ -30,6 +30,7 @@
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <sys/vfs.h>
+#include <sys/wait.h>
 #include <sys/personality.h>
 #include <grp.h>
 #include <unistd.h>
@@ -4489,7 +4490,8 @@ flatpak_run_app (FlatpakDecomposed *app_ref,
   commandline = flatpak_quote_argv ((const char **) bwrap->argv->pdata, -1);
   g_debug ("Running '%s'", commandline);
 
-  if ((flags & FLATPAK_RUN_FLAG_BACKGROUND) != 0)
+  if ((flags & (FLATPAK_RUN_FLAG_BACKGROUND)) != 0 ||
+      g_getenv ("FLATPAK_TEST_COVERAGE") != NULL)
     {
       GPid child_pid;
       char pid_str[64];
@@ -4497,7 +4499,8 @@ flatpak_run_app (FlatpakDecomposed *app_ref,
       GSpawnFlags spawn_flags;
 
       spawn_flags = G_SPAWN_SEARCH_PATH;
-      if (flags & FLATPAK_RUN_FLAG_DO_NOT_REAP)
+      if (flags & FLATPAK_RUN_FLAG_DO_NOT_REAP ||
+          (flags & FLATPAK_RUN_FLAG_BACKGROUND) == 0)
         spawn_flags |= G_SPAWN_DO_NOT_REAP_CHILD;
 
       /* We use LEAVE_DESCRIPTORS_OPEN to work around dead-lock, see flatpak_close_fds_workaround */
@@ -4521,6 +4524,22 @@ flatpak_run_app (FlatpakDecomposed *app_ref,
       g_snprintf (pid_str, sizeof (pid_str), "%d", child_pid);
       pid_path = g_build_filename (instance_id_host_dir, "pid", NULL);
       g_file_set_contents (pid_path, pid_str, -1, NULL);
+
+      if ((flags & (FLATPAK_RUN_FLAG_BACKGROUND)) == 0)
+        {
+          int wait_status;
+
+          if (waitpid (child_pid, &wait_status, 0) != child_pid)
+            return glnx_throw_errno_prefix (error, "Failed to wait for child process");
+
+          if (WIFEXITED (wait_status))
+            exit (WEXITSTATUS (wait_status));
+
+          if (WIFSIGNALED (wait_status))
+            exit (128 + WTERMSIG (wait_status));
+
+          return glnx_throw (error, "Unknown wait status from waitpid(): %d", wait_status);
+        }
     }
   else
     {

Makefile.am
@@ -210,7 +210,7 @@ AM_DISTCHECK_CONFIGURE_FLAGS =		\
 
 coverage:
 	$(AM_V_GEN) $(MAKE) $(AM_MAKEFLAGS) lcov-clean
-	$(AM_V_GEN) $(MAKE) check
+	$(AM_V_GEN) FLATPAK_TEST_COVERAGE=1 $(MAKE) check
 	$(AM_V_GEN) $(MAKE) $(AM_MAKEFLAGS) genlcov
 
 lcov-clean:

commit 43e033e45a2fcf12d2852b0c6ca5d36f42549d82

Always allow app to inherit redirected fds from flatpak-run(1)
As noticed on #5615, under normal circumstances, flatpak-run(1)
replaces itself with the bwrap process via execve(), and does not
close any fds that it might have inherited from its parent. This
allows for patterns like:

    flatpak run com.example.App --in-fd=3 --out-fd=5 3<foo 5>bar

However, using execve() is annoying when trying to analyze code
coverage, because the coverage instrumentation does not get the
opportunity to write out its data during exit, so it is possible to
set FLATPAK_TEST_COVERAGE=1 to make flatpak run the app as a child
process and wait for it. This puts us on the code path normally used
for apps launched in the background by flatpak_installation_launch_full(),
which *don't* inherit arbitrary fds from their parent.

Detect this situation and use a different child setup function,
avoiding closing fds that we were meant to inherit.

Fixes: 88a928ea "run: Avoid execve() when measuring test coverage"
Signed-off-by: Simon McVittie <smcv@collabora.com>

common/flatpak-bwrap-private.h
@@ -90,6 +90,7 @@ void          flatpak_bwrap_populate_runtime_dir (FlatpakBwrap *bwrap,
                                                   const char *shared_xdg_runtime_dir);
 
 void          flatpak_bwrap_child_setup_cb (gpointer user_data);
+void          flatpak_bwrap_child_setup_inherit_fds_cb (gpointer user_data);
 void          flatpak_bwrap_child_setup (GArray *fd_array,
                                          gboolean close_fd_workaround);
 

common/flatpak-bwrap.c
@@ -530,6 +530,16 @@ flatpak_bwrap_child_setup_cb (gpointer user_data)
   flatpak_bwrap_child_setup (fd_array, TRUE);
 }
 
+/* Unset FD_CLOEXEC on the array of fds passed in @user_data,
+ * but do not set FD_CLOEXEC on all other fds */
+void
+flatpak_bwrap_child_setup_inherit_fds_cb (gpointer user_data)
+{
+  GArray *fd_array = user_data;
+
+  flatpak_bwrap_child_setup (fd_array, FALSE);
+}
+
 /* Add a --sync-fd argument for bwrap(1). Returns the write end of the pipe on
  * success, or -1 on error. */
 int

common/flatpak-run.c
@@ -3441,11 +3441,17 @@ flatpak_run_app (FlatpakDecomposed   *app_ref,
       char pid_str[64];
       g_autofree char *pid_path = NULL;
       GSpawnFlags spawn_flags;
+      GSpawnChildSetupFunc child_setup;
 
       spawn_flags = G_SPAWN_SEARCH_PATH;
       if (flags & FLATPAK_RUN_FLAG_DO_NOT_REAP ||
           (flags & FLATPAK_RUN_FLAG_BACKGROUND) == 0)
         spawn_flags |= G_SPAWN_DO_NOT_REAP_CHILD;
+
+      if (flags & FLATPAK_RUN_FLAG_BACKGROUND)
+        child_setup = flatpak_bwrap_child_setup_cb;
+      else
+        child_setup = flatpak_bwrap_child_setup_inherit_fds_cb;
 
       /* We use LEAVE_DESCRIPTORS_OPEN to work around dead-lock, see flatpak_close_fds_workaround */
       spawn_flags |= G_SPAWN_LEAVE_DESCRIPTORS_OPEN;
@@ -3460,7 +3466,7 @@ flatpak_run_app (FlatpakDecomposed   *app_ref,
                           (char **) bwrap->argv->pdata,
                           bwrap->envp,
                           spawn_flags,
-                          flatpak_bwrap_child_setup_cb, bwrap->fds,
+                          child_setup, bwrap->fds,
                           &child_pid,
                           error))
         return FALSE;