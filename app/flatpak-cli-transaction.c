flatpak/flatpak

commit 6e3cc82af3c0ed0f328bd356b6a770d0b6418412

Fix memory leaks
Build with address sanitizer and run the tests

Signed-off-by: Hubert Figuière <hub@figuiere.net>

app/flatpak-cli-transaction.c
@@ -159,7 +159,7 @@ install_authenticator (FlatpakTransaction            *old_transaction,
   FlatpakCliTransaction *old_cli = FLATPAK_CLI_TRANSACTION (old_transaction);
   g_autoptr(FlatpakTransaction)  transaction2 = NULL;
   g_autoptr(GError) local_error = NULL;
-  FlatpakInstallation *installation = flatpak_transaction_get_installation (old_transaction);
+  g_autoptr(FlatpakInstallation) installation = flatpak_transaction_get_installation (old_transaction);
   FlatpakDir *dir = flatpak_installation_get_dir (installation, NULL);
 
   if (dir == NULL)
@@ -888,7 +888,7 @@ end_of_lifed_with_rebase (FlatpakTransaction *transaction,
   EolAction action = EOL_UNDECIDED;
   EolAction old_action = EOL_UNDECIDED;
   gboolean can_rebase = rebased_to_ref != NULL && remote != NULL;
-  FlatpakInstallation *installation = flatpak_transaction_get_installation (transaction);
+  g_autoptr(FlatpakInstallation) installation = flatpak_transaction_get_installation (transaction);
   FlatpakDir *dir = flatpak_installation_get_dir (installation, NULL);
 
   if (ref == NULL)
@@ -1287,7 +1287,7 @@ static gboolean
 transaction_ready_pre_auth (FlatpakTransaction *transaction)
 {
   FlatpakCliTransaction *self = FLATPAK_CLI_TRANSACTION (transaction);
-  GList *ops = flatpak_transaction_get_operations (transaction);
+  g_autolist(FlatpakTransactionOperation) ops = flatpak_transaction_get_operations (transaction);
   GList *l;
   int i;
   FlatpakTablePrinter *printer;

app/flatpak-builtins-create-usb.c
@@ -431,6 +431,7 @@ ostree_create_usb (GOptionContext *context,
               break;
             }
         }
+      glnx_dirfd_iterator_clear (&repos_iter);
 
       /* If we need a symlink, find a unique name for it and create it. */
       if (need_symlink)

app/flatpak-quiet-transaction.c
@@ -190,7 +190,7 @@ install_authenticator (FlatpakTransaction            *old_transaction,
 {
   g_autoptr(FlatpakTransaction)  transaction2 = NULL;
   g_autoptr(GError) local_error = NULL;
-  FlatpakInstallation *installation = flatpak_transaction_get_installation (old_transaction);
+  g_autoptr(FlatpakInstallation) installation = flatpak_transaction_get_installation (old_transaction);
   FlatpakDir *dir = flatpak_installation_get_dir (installation, NULL);
 
   if (dir == NULL)

common/flatpak-dir.c
@@ -11513,7 +11513,7 @@ flatpak_dir_get_if_deployed (FlatpakDir        *self,
     }
 
   if (g_file_query_file_type (deploy_dir, G_FILE_QUERY_INFO_NONE, cancellable) == G_FILE_TYPE_DIRECTORY)
-    return g_object_ref (deploy_dir);
+    return g_steal_pointer (&deploy_dir);
 
   /* Maybe it was removed but is still living? */
   if (checksum != NULL)
@@ -11527,7 +11527,7 @@ flatpak_dir_get_if_deployed (FlatpakDir        *self,
       removed_deploy_dir = g_file_get_child (removed_dir, dirname);
 
       if (g_file_query_file_type (removed_deploy_dir, G_FILE_QUERY_INFO_NONE, cancellable) == G_FILE_TYPE_DIRECTORY)
-        return g_object_ref (removed_deploy_dir);
+        return g_steal_pointer (&removed_deploy_dir);
     }
 
   return NULL;

common/flatpak-locale-utils.c
@@ -298,7 +298,7 @@ flatpak_get_locale_langs_from_accounts_dbus_for_user (GDBusProxy *proxy, GPtrArr
       value = g_dbus_proxy_get_cached_property (accounts_proxy, "Languages");
       if (value != NULL)
         {
-          const char **locales = g_variant_get_strv (value, NULL);
+          g_autofree const char **locales = g_variant_get_strv (value, NULL);
           guint i;
 
           for (i = 0; locales != NULL && locales[i] != NULL; i++)

common/flatpak-prune.c
@@ -775,6 +775,7 @@ flatpak_repo_prune (OstreeRepo    *repo,
 
     g_timer_stop (timer);
     g_info ("Elapsed time: %.1f sec",  g_timer_elapsed (timer, NULL));
+    g_clear_pointer (&timer, g_timer_destroy);
   }
 
   {

common/flatpak-transaction.c
@@ -3941,9 +3941,11 @@ request_tokens_for_remote (FlatpakTransaction *self,
       g_autoptr(GFile) deploy = NULL;
       deploy = flatpak_dir_get_if_deployed (priv->dir, auto_install_ref, NULL, cancellable);
       if (deploy == NULL)
-        g_signal_emit (self, signals[INSTALL_AUTHENTICATOR], 0,
-                       remote, flatpak_decomposed_get_ref (auto_install_ref));
-      deploy = flatpak_dir_get_if_deployed (priv->dir, auto_install_ref, NULL, cancellable);
+        {
+          g_signal_emit (self, signals[INSTALL_AUTHENTICATOR], 0,
+                         remote, flatpak_decomposed_get_ref (auto_install_ref));
+          deploy = flatpak_dir_get_if_deployed (priv->dir, auto_install_ref, NULL, cancellable);
+        }
       if (deploy == NULL)
         return flatpak_fail (error, _("No authenticator installed for remote '%s'"), remote);
     }
@@ -4024,7 +4026,7 @@ request_tokens_for_remote (FlatpakTransaction *self,
   g_assert (priv->active_request_id == 0); /* No outstanding requests */
   priv->active_request = NULL;
 
-  results = data.results; /* Make sure its freed as needed */
+  results = data.results; /* Make sure it's freed as needed */
 
   {
     g_autofree char *results_str = results != NULL ? g_variant_print (results, FALSE) : g_strdup ("NULL");
@@ -4082,6 +4084,7 @@ request_tokens_for_remote (FlatpakTransaction *self,
               token = token_for_refs;
               break;
             }
+          g_clear_pointer (&refs_strv, g_free);
         }
 
       if (token == NULL)

common/flatpak-utils.c
@@ -633,7 +633,7 @@ flatpak_get_bwrap (void)
 gboolean
 flatpak_bwrap_is_unprivileged (void)
 {
-  const char *path = g_find_program_in_path (flatpak_get_bwrap ());
+  g_autofree char *path = g_find_program_in_path (flatpak_get_bwrap ());
   struct stat st;
 
   /* Various features are supported only if bwrap exists and is not setuid */
@@ -3530,6 +3530,7 @@ _ostree_repo_static_delta_superblock_digest (OstreeRepo    *repo,
   glnx_autofd int fd = -1;
   guint8 digest[OSTREE_SHA256_DIGEST_LEN];
   gsize len;
+  gpointer data = NULL;
 
   if (!glnx_openat_rdonly (ostree_repo_get_dfd (repo), superblock, TRUE, &fd, error))
     return NULL;
@@ -3543,9 +3544,10 @@ _ostree_repo_static_delta_superblock_digest (OstreeRepo    *repo,
   len = sizeof digest;
   g_checksum_get_digest (checksum, digest, &len);
 
+  data = g_memdup2 (digest, len);
   return g_variant_new_from_data (G_VARIANT_TYPE ("ay"),
-                                  g_memdup2 (digest, len), len,
-                                  FALSE, g_free, FALSE);
+                                  data, len,
+                                  FALSE, g_free, data);
 }
 
 static char *
@@ -4630,7 +4632,7 @@ flatpak_repo_gc_digested_summaries (OstreeRepo *repo,
         {
           if (strcmp (ext, ".gz") == 0 && strlen (dent->d_name) == 64 + 3)
             {
-              char *sha256 = g_strndup (dent->d_name, 64);
+              g_autofree char *sha256 = g_strndup (dent->d_name, 64);
 
               /* Keep all the referenced summaries */
               if (g_hash_table_contains (digested_summary_cache, sha256))
@@ -4646,7 +4648,7 @@ flatpak_repo_gc_digested_summaries (OstreeRepo *repo,
               const char *dash = strchr (dent->d_name, '-');
               if (dash != NULL && dash < ext && (ext - dash) == 1 + 64)
                 {
-                  char *to_sha256 = g_strndup (dash + 1, 64);
+                  g_autofree char *to_sha256 = g_strndup (dash + 1, 64);
 
                   /* Only keep deltas going to a generated summary */
                   if (g_hash_table_contains (digested_summaries, to_sha256))

commit 84a0e51850f0da0f2f5fee1e1d2bf36702becbdd

cli-transaction: Do not double free transaction ops
The transaction ops are a g_autolist(FlatpakTransactionOperation) which
means the list gets freed automatically. Calling g_list_free_full
without clearing the variable to NULL results in a double-free.

Closes: https://github.com/flatpak/flatpak/issues/5763
Fixes: 6e3cc82a ("Fix memory leaks")
Signed-off-by: Sebastian Wick <sebastian.wick@redhat.com>

app/flatpak-cli-transaction.c
@@ -1467,10 +1467,7 @@ transaction_ready_pre_auth (FlatpakTransaction *transaction)
         ret = flatpak_yes_no_prompt (TRUE, _("Proceed with these changes to the %s?"), name);
 
       if (!ret)
-        {
-          g_list_free_full (ops, g_object_unref);
-          return FALSE;
-        }
+        return FALSE;
     }
   else
     g_print ("\n\n");