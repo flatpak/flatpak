3ededee9c17bf5d19972ae9a25edf120538eb646
JakobDev <jakobdev@gmx.de>
2025-02-25 16:08:51 +0100

    build-finish: Export AppStream external release metainfo
    The appstream docs say:
    
      Release data may be present directly in a component metainfo file, but
      also optionally be split out into an external metadata file.
    
      The file must be installed as
      /usr/share/metainfo/releases/%{cid}.releases.xml, where cid is the
      component ID of the component the release information belongs to.
    
    This change allows flatpak build-finish to export those external release
    files.
    
    Closes #6057

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -8109,6 +8109,7 @@ flatpak_export_dir (GFile        *source,
     "share/krunner/dbusplugins",           "../../..",
     "share/mime/packages",                 "../../..",
     "share/metainfo",                      "../..",
+    "share/metainfo/releases",             "../../..",
     "bin",                                 "..",
   };
   int i;
