621790e7978382878253047507938f23054c8ebd
Sebastian Wick <sebastian.wick@redhat.com>
2025-08-28 13:44:59 +0200

    transaction: Support sideloading from OCI repos/archives
    This allows adding FlatpakImageCollection sideload repositories to a
    transaction.

--- common/flatpak-transaction.h
+++ common/flatpak-transaction.h
@@ -272,6 +272,11 @@ FLATPAK_EXTERN
 void                flatpak_transaction_add_sideload_repo (FlatpakTransaction  *self,
                                                            const char          *path);
 FLATPAK_EXTERN
+gboolean            flatpak_transaction_add_sideload_image_collection (FlatpakTransaction  *self,
+                                                                       const char          *location,
+                                                                       GCancellable        *cancellable,
+                                                                       GError             **error);
+FLATPAK_EXTERN
 void                flatpak_transaction_add_default_dependency_sources (FlatpakTransaction *self);
 FLATPAK_EXTERN
 gboolean            flatpak_transaction_run (FlatpakTransaction *transaction,
