76cb5c793eb44155663f1452189c667638496cfb
Sebastian Wick <sebastian.wick@redhat.com>
2025-11-20 22:07:23 +0100

    validate-icon: Do not leak a GError instance

--- icon-validator/validate-icon.c
+++ icon-validator/validate-icon.c
@@ -253,7 +253,7 @@ int
 main (int argc, char *argv[])
 {
   g_autoptr(GOptionContext) context = NULL;
-  GError *error = NULL;
+  g_autoptr(GError) error = NULL;
 
   context = g_option_context_new ("WIDTH HEIGHT PATH");
   g_option_context_add_main_entries (context, entries, NULL);
