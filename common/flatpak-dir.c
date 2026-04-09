flatpak/flatpak

commit ac62ebe3080401501efb47044981d2b519629186

run: Use O_PATH fds for the runtime and app deploy directories
This also allows us to use glnx_chaseat, and other at-functions to
traverse the filesystem tree in a safe way.

This is important because the app and runtime deploy directories can be
under an attackers control. The flatpak portal for example allows
sandboxed apps to provide them.

In particular, attacks where the deploy dirs get replaced by a symlink
pointing into the host system will be stopped by this.

Note that this change alone is not enough to avoid the attack, and the
portal has to be changed as well.

common/flatpak-dir.c
@@ -9316,7 +9316,13 @@ apply_extra_data (FlatpakDir   *self,
    * Disable /proc entirely in this context. */
   run_flags |= FLATPAK_RUN_FLAG_NO_PROC;
 
-  if (!flatpak_run_setup_base_argv (bwrap, runtime_files, NULL, runtime_arch,
+  glnx_autofd int usr_fd = -1;
+  usr_fd = open (flatpak_file_get_path_cached (runtime_files),
+                 O_PATH | O_CLOEXEC | O_NOFOLLOW);
+  if (usr_fd < 0)
+    return glnx_throw_errno_prefix (error, "Failed to open runtime files");
+
+  if (!flatpak_run_setup_base_argv (bwrap, usr_fd, NULL, runtime_arch,
                                     run_flags, error))
     return FALSE;
 

app/flatpak-builtins-build.c
@@ -516,7 +516,13 @@ flatpak_builtin_build (int argc, char **argv, GCancellable *cancellable, GError 
   /* Never set up an a11y bus for builds */
   run_flags |= FLATPAK_RUN_FLAG_NO_A11Y_BUS_PROXY;
 
-  if (!flatpak_run_setup_base_argv (bwrap, runtime_files, app_id_dir, arch,
+  glnx_autofd int usr_fd = -1;
+  usr_fd = open (flatpak_file_get_path_cached (runtime_files),
+                 O_PATH | O_CLOEXEC | O_NOFOLLOW);
+  if (usr_fd < 0)
+    return glnx_throw_errno_prefix (error, "Failed to open runtime files");
+
+  if (!flatpak_run_setup_base_argv (bwrap, usr_fd, app_id_dir, arch,
                                     run_flags, error))
     return FALSE;
 

app/flatpak-builtins-run.c
@@ -113,6 +113,8 @@ flatpak_builtin_run (int argc, char **argv, GCancellable *cancellable, GError **
   g_autoptr(GError) local_error = NULL;
   g_autoptr(GPtrArray) dirs = NULL;
   FlatpakRunFlags flags = 0;
+  glnx_autofd int app_fd = -1;
+  glnx_autofd int usr_fd = -1;
 
   run_environ = g_get_environ ();
 
@@ -313,14 +315,45 @@ flatpak_builtin_run (int argc, char **argv, GCancellable *cancellable, GError **
   if (!opt_clear_env)
     flags |= FLATPAK_RUN_FLAG_CLEAR_ENV;
 
+  if (opt_app_path != NULL)
+    {
+      if (g_strcmp0 (opt_app_path, "") == 0)
+        {
+          app_fd = FLATPAK_RUN_APP_DEPLOY_APP_EMPTY;
+        }
+      else
+        {
+          app_fd = open (opt_app_path, O_PATH | O_CLOEXEC | O_NOFOLLOW);
+
+          if (app_fd < 0)
+            return glnx_throw_errno_prefix (error, "Failed to open app-path");
+        }
+    }
+  else
+    {
+      app_fd = FLATPAK_RUN_APP_DEPLOY_APP_ORIGINAL;
+    }
+
+  if (opt_usr_path != NULL)
+    {
+      usr_fd = open (opt_usr_path, O_PATH | O_CLOEXEC | O_NOFOLLOW);
+
+      if (usr_fd < 0)
+        return glnx_throw_errno_prefix (error, "Failed to open usr-path");
+    }
+  else
+    {
+      usr_fd = FLATPAK_RUN_APP_DEPLOY_USR_ORIGINAL;
+    }
+
   if (!flatpak_run_app (app_deploy ? app_ref : runtime_ref,
                         app_deploy,
-                        opt_app_path,
+                        app_fd,
                         arg_context,
                         opt_runtime,
                         opt_runtime_version,
                         opt_runtime_commit,
-                        opt_usr_path,
+                        usr_fd,
                         opt_parent_pid,
                         flags,
                         opt_cwd,

common/flatpak-installation.c
@@ -700,9 +700,10 @@ flatpak_installation_launch_full (FlatpakInstallation *self,
 
   if (!flatpak_run_app (app_ref,
                         app_deploy,
+                        FLATPAK_RUN_APP_DEPLOY_APP_ORIGINAL,
                         NULL,
-                        NULL, NULL,
                         NULL, NULL, NULL,
+                        FLATPAK_RUN_APP_DEPLOY_USR_ORIGINAL,
                         0,
                         run_flags,
                         NULL,

common/flatpak-run-private.h
@@ -30,6 +30,11 @@
 #include "flatpak-utils-private.h"
 #include "flatpak-exports-private.h"
 
+#define FLATPAK_RUN_APP_DEPLOY_APP_ORIGINAL (-2)
+#define FLATPAK_RUN_APP_DEPLOY_APP_EMPTY (-3)
+
+#define FLATPAK_RUN_APP_DEPLOY_USR_ORIGINAL (-2)
+
 gboolean flatpak_run_in_transient_unit (const char *app_id,
                                         const char *instance_id,
                                         GError    **error);
@@ -76,7 +81,7 @@ gboolean flatpak_ensure_data_dir (GFile        *app_id_dir,
                                   GError      **error);
 
 gboolean flatpak_run_setup_base_argv (FlatpakBwrap   *bwrap,
-                                      GFile          *runtime_files,
+                                      int             runtime_fd,
                                       GFile          *app_id_dir,
                                       const char     *arch,
                                       FlatpakRunFlags flags,
@@ -109,12 +114,12 @@ gboolean flatpak_run_add_app_info_args (FlatpakBwrap           *bwrap,
 
 gboolean flatpak_run_app (FlatpakDecomposed   *app_ref,
                           FlatpakDeploy       *app_deploy,
-                          const char          *custom_app_path,
+                          int                  custom_app_fd,
                           FlatpakContext      *extra_context,
                           const char          *custom_runtime,
                           const char          *custom_runtime_version,
                           const char          *custom_runtime_commit,
-                          const char          *custom_usr_path,
+                          int                  custom_usr_fd,
                           int                  parent_pid,
                           FlatpakRunFlags      flags,
                           const char          *cwd,

commit dc3932f2de8c9e09679872a98c27c4e17d9e50d1

dir: In apply_extra_data(), don't assume there is always a runtime
org.freedesktop.Platform.openh264 is one example of an extension that
runs a statically-linked extra-data helper, with no runtime. Only open
the runtime if there is one.

Fixes: ac62ebe3 "run: Use O_PATH fds for the runtime and app deploy directories"
Resolves: https://github.com/flatpak/flatpak/issues/6583
Signed-off-by: Simon McVittie <smcv@collabora.com>

common/flatpak-dir.c
@@ -9317,10 +9317,14 @@ apply_extra_data (FlatpakDir   *self,
   run_flags |= FLATPAK_RUN_FLAG_NO_PROC;
 
   glnx_autofd int usr_fd = -1;
-  usr_fd = open (flatpak_file_get_path_cached (runtime_files),
-                 O_PATH | O_CLOEXEC | O_NOFOLLOW);
-  if (usr_fd < 0)
-    return glnx_throw_errno_prefix (error, "Failed to open runtime files");
+
+  if (runtime_files != NULL)
+    {
+      usr_fd = open (flatpak_file_get_path_cached (runtime_files),
+                     O_PATH | O_CLOEXEC | O_NOFOLLOW);
+      if (usr_fd < 0)
+        return glnx_throw_errno_prefix (error, "Failed to open runtime files");
+    }
 
   if (!flatpak_run_setup_base_argv (bwrap, usr_fd, NULL, runtime_arch,
                                     run_flags, error))