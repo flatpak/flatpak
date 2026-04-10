flatpak/flatpak

commit 1b5e886d7f22f2ee81c2149c04e915bf4cabe518

run: Add --usr-fd and --app-fd options
Exposes options to pass in a fd for the runtime and app deploy. The
flatpak portal will make use of this in a following commit.

app/flatpak-builtins-run.c
@@ -60,7 +60,9 @@ static gboolean opt_parent_expose_pids;
 static gboolean opt_parent_share_pids;
 static int opt_instance_id_fd = -1;
 static char *opt_app_path;
+static int opt_app_fd = -1;
 static char *opt_usr_path;
+static int opt_usr_fd = -1;
 static gboolean opt_clear_env;
 
 static GOptionEntry options[] = {
@@ -89,7 +91,9 @@ static GOptionEntry options[] = {
   { "parent-share-pids", 0, 0, G_OPTION_ARG_NONE, &opt_parent_share_pids, N_("Share process ID namespace with parent"), NULL },
   { "instance-id-fd", 0, 0, G_OPTION_ARG_INT, &opt_instance_id_fd, N_("Write the instance ID to the given file descriptor"), NULL },
   { "app-path", 0, 0, G_OPTION_ARG_FILENAME, &opt_app_path, N_("Use PATH instead of the app's /app"), N_("PATH") },
+  { "app-fd", 0, 0, G_OPTION_ARG_INT, &opt_app_fd, N_("Use FD instead of the app's /app"), N_("FD") },
   { "usr-path", 0, 0, G_OPTION_ARG_FILENAME, &opt_usr_path, N_("Use PATH instead of the runtime's /usr"), N_("PATH") },
+  { "usr-fd", 0, 0, G_OPTION_ARG_INT, &opt_usr_fd, N_("Use FD instead of the runtime's /usr"), N_("FD") },
   { "clear-env", 0, 0, G_OPTION_ARG_NONE, &opt_clear_env, N_("Clear all outside environment variables"), NULL },
   { NULL }
 };
@@ -315,7 +319,18 @@ flatpak_builtin_run (int argc, char **argv, GCancellable *cancellable, GError **
   if (!opt_clear_env)
     flags |= FLATPAK_RUN_FLAG_CLEAR_ENV;
 
-  if (opt_app_path != NULL)
+  if (opt_app_fd >= 0 && opt_app_path != NULL)
+    {
+      flatpak_fail_error (error, FLATPAK_ERROR,
+                          _("app-fd and app-path cannot both be used"));
+      return FALSE;
+    }
+
+  if (opt_app_fd >= 0)
+    {
+      app_fd = opt_app_fd;
+    }
+  else if (opt_app_path != NULL)
     {
       if (g_strcmp0 (opt_app_path, "") == 0)
         {
@@ -334,7 +349,18 @@ flatpak_builtin_run (int argc, char **argv, GCancellable *cancellable, GError **
       app_fd = FLATPAK_RUN_APP_DEPLOY_APP_ORIGINAL;
     }
 
-  if (opt_usr_path != NULL)
+  if (opt_usr_fd >= 0 && opt_usr_path != NULL)
+    {
+      flatpak_fail_error (error, FLATPAK_ERROR,
+                          _("usr-fd and usr-path cannot both be used"));
+      return FALSE;
+    }
+
+  if (opt_usr_fd >= 0)
+    {
+      usr_fd = opt_usr_fd;
+    }
+  else if (opt_usr_path != NULL)
     {
       usr_fd = open (opt_usr_path, O_PATH | O_CLOEXEC | O_NOFOLLOW);
 

commit e0cd58169610a38737b3da446ad6b45ba5745a9f

run, context: Mark fd arguments as close-on-exec
On entry to `flatpak run`, these fds have been inheritable (not
FD_CLOEXEC), otherwise they would not have been inherited; but we don't
want the "payload" command to inherit them, so set them as
non-close-on-exec as soon as we receive them. In the cases where we pass
them down to the underlying bwrap command, we'll either dup them, or
set them to be inheritable again (in practice we dup them).

In particular, Chromium-derived web browsers get very upset when their
subsandbox processes inherit unexpected fds, which has been causing crashes
with no useful diagnostic information since CVE-2026-34078 was fixed.

Fixes: 1b5e886d "run: Add --usr-fd and --app-fd options"
Fixes: b5ae89ed "run: Add --(ro-)bind-fd options"
Resolves: https://github.com/flatpak/flatpak/issues/6582
Signed-off-by: Simon McVittie <smcv@collabora.com>

app/flatpak-builtins-run.c
@@ -82,6 +82,9 @@ option_bind_fd_cb (const char  *option_name,
   if (fd < 3)
     return glnx_throw (error, "File descriptors 0, 1, 2 are reserved");
 
+  if (!flatpak_set_cloexec (fd))
+    return glnx_throw_errno_prefix (error, "--bind-fd");
+
   g_array_append_val (opt_bind_fds, fd);
   fd = -1; /* ownership transferred to GArray */
   return TRUE;
@@ -101,6 +104,9 @@ option_ro_bind_fd_cb (const char  *option_name,
 
   if (fd < 3)
     return glnx_throw (error, "File descriptors 0, 1, 2 are reserved");
+
+  if (!flatpak_set_cloexec (fd))
+    return glnx_throw_errno_prefix (error, "--ro-bind-fd");
 
   g_array_append_val (opt_ro_bind_fds, fd);
   fd = -1; /* ownership transferred to GArray */
@@ -122,6 +128,9 @@ opt_instance_id_fd_cb (const char  *option_name,
   if (fd < 3)
     return glnx_throw (error, "File descriptors 0, 1, 2 are reserved");
 
+  if (!flatpak_set_cloexec (fd))
+    return glnx_throw_errno_prefix (error, "--instance-id-fd");
+
   opt_instance_id_fd = g_steal_fd (&fd);
   return TRUE;
 }
@@ -141,6 +150,9 @@ opt_app_fd_cb (const char  *option_name,
   if (fd < 3)
     return glnx_throw (error, "File descriptors 0, 1, 2 are reserved");
 
+  if (!flatpak_set_cloexec (fd))
+    return glnx_throw_errno_prefix (error, "--app-fd");
+
   opt_app_fd = g_steal_fd (&fd);
   return TRUE;
 }
@@ -159,6 +171,9 @@ opt_usr_fd_cb (const char  *option_name,
 
   if (fd < 3)
     return glnx_throw (error, "File descriptors 0, 1, 2 are reserved");
+
+  if (!flatpak_set_cloexec (fd))
+    return glnx_throw_errno_prefix (error, "--usr-fd");
 
   opt_usr_fd = g_steal_fd (&fd);
   return TRUE;

common/flatpak-context.c
@@ -2441,6 +2441,14 @@ option_env_fd_cb (const gchar *option_name,
   if (fd < 3)
     return glnx_throw (error, "File descriptors 0, 1, 2 are reserved");
 
+  /* This is not strictly necessary, because we're going to close it after
+   * parsing the environment block, but let's be consistent with other fd
+   * arguments that we need to avoid being inherited by the "payload"
+   * command. This is also a convenient place to validate that it's an
+   * open fd. */
+  if (!flatpak_set_cloexec (fd))
+    return glnx_throw_errno_prefix (error, "--env-fd");
+
   return flatpak_context_parse_env_fd (context, fd, error);
 }
 