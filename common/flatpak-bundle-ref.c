13cf8cfd278df4ebcbfdd61d3a3b311072cac48a
Sebastian Wick <sebastian.wick@redhat.com>
2026-07-02 19:56:34 +0200

    bundle-ref: Use g_clear_pointer for nullable GBytes fields
    The metadata, appstream, icon_64, and icon_128 fields are conditionally
    initialized in flatpak_bundle_ref_new() depending on which keys exist in
    the bundle metadata. The finalize function used g_bytes_unref() which is
    not NULL-safe on older GLib versions.

--- common/flatpak-bundle-ref.c
+++ common/flatpak-bundle-ref.c
@@ -66,10 +66,10 @@ flatpak_bundle_ref_finalize (GObject *object)
 
   g_clear_object (&priv->file);
 
-  g_bytes_unref (priv->metadata);
-  g_bytes_unref (priv->appstream);
-  g_bytes_unref (priv->icon_64);
-  g_bytes_unref (priv->icon_128);
+  g_clear_pointer (&priv->metadata, g_bytes_unref);
+  g_clear_pointer (&priv->appstream, g_bytes_unref);
+  g_clear_pointer (&priv->icon_64, g_bytes_unref);
+  g_clear_pointer (&priv->icon_128, g_bytes_unref);
   g_free (priv->origin);
   g_free (priv->runtime_repo);
 
