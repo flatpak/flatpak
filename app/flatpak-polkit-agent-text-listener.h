3216f8eaf85eef8577b7bab8e41a9683cfd4b27b
Maximiliano Sandoval <msandova@protonmail.com>
2026-07-04 15:31:38 +0200

    common: Stop using G_GNUC_CONST in _get_type funcs
    As per g_type_ensure's documentation it is technically incorrect to mark
    _get_type fns with G_GNUC_CONST since they have side-effects on their
    first run.
    
    See https://gitlab.gnome.org/GNOME/glib/-/merge_requests/5223 for more
    details.
    
    (cherry picked from commit 59f9a7729f9de3c7d3caf2d27390fb9e2cfb6882):

--- app/flatpak-polkit-agent-text-listener.h
+++ app/flatpak-polkit-agent-text-listener.h
@@ -13,7 +13,7 @@ typedef struct _FlatpakPolkitAgentTextListener FlatpakPolkitAgentTextListener;
 #define FLATPAK_POLKIT_AGENT_TEXT_LISTENER(o)            (G_TYPE_CHECK_INSTANCE_CAST ((o), FLATPAK_POLKIT_AGENT_TYPE_TEXT_LISTENER, FlatpakPolkitAgentTextListener))
 #define FLATPAK_POLKIT_AGENT_IS_TEXT_LISTENER(o)         (G_TYPE_CHECK_INSTANCE_TYPE ((o), FLATPAK_POLKIT_AGENT_TYPE_TEXT_LISTENER))
 
-GType                flatpak_polkit_agent_text_listener_get_type (void) G_GNUC_CONST;
+GType                flatpak_polkit_agent_text_listener_get_type (void);
 PolkitAgentListener *flatpak_polkit_agent_text_listener_new (GCancellable   *cancellable,
                                                              GError        **error);
 
