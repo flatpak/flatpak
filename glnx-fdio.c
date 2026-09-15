324a09f2128b8f812c7f557095aec1e3a6782010
Simon McVittie <smcv@collabora.com>
2026-09-15 13:32:49 +0100

    fdio: Don't try to use fd-relative operations to set symlink xattrs
    There is currently no kernel interface to set extended attributes on
    a symbolic link by its O_PATH fd: fsetxattr() on the O_PATH fd itself
    fails because we do not have write access, if we reopen the fd via
    `/proc/self/fd/N` with O_NOFOLLOW and then use fsetxattr() then that
    will attempt to set the xattrs on the magic symlink in /proc instead of
    on the symlink we originally created, and if we reopen the fd via
    `/proc/self/fd/N` without O_NOFOLLOW then that will attempt to set the
    xattrs on the symlink's target. The best we can do is to call
    lsetxattr() relative to the destination directory.
    
    This partially reverts commit 6bb03208 "glnx_file_copy_at: Use O_PATH fd
    for xattr and ownership operations".
    
    Helps: https://github.com/flatpak/flatpak/issues/6818
    Co-authored-by:Sebastian Wick <sebastian.wick@redhat.com>
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- glnx-fdio.c
+++ glnx-fdio.c
@@ -735,13 +735,19 @@ copy_symlink_at (int                   src_dfd,
 
   if (!(copyflags & GLNX_FILE_COPY_NOXATTRS))
     {
+      /* The only way to do set xattrs on a symlink is via lsetxattr. An O_PATH
+       * is just not enough proof to modify the file (such as setting the
+       * xattrs) but it's the only non-racy reference to a symlink. So setting
+       * the xattrs on a symlink will be racy.
+       */
       g_autoptr(GVariant) xattrs = NULL;
 
       if (!glnx_dfd_name_get_all_xattrs (src_dfd, src_subpath, &xattrs,
                                          cancellable, error))
         return FALSE;
 
-      if (!glnx_fd_set_all_xattrs (fd, xattrs, cancellable, error))
+      if (!glnx_dfd_name_set_all_xattrs (dest_dfd, dest_subpath, xattrs,
+                                         cancellable, error))
         return FALSE;
     }
 
