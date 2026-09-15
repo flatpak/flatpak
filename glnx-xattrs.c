f05eb4319c0a288134c54f640fdb44ec4ad11753
Simon McVittie <smcv@collabora.com>
2026-09-15 13:05:36 +0100

    Revert "xattrs: Use /proc/self/fd path in glnx_fd_get_all_xattrs"
    We can't use fgetxattr() to get the extended attributes of a symlink via
    the magic symlink in /proc/self/fd, because opening with O_NOFOLLOW
    would give us the extended attributes of the magic symlink
    /proc/self/fd/N, and opening without O_NOFOLLOW would dereference the
    symlink.
    
    This reverts commit fc9387a79b0a8ac739209878c400b40675be28df.
    
    Helps: https://github.com/flatpak/flatpak/issues/6818
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- glnx-xattrs.c
+++ glnx-xattrs.c
@@ -229,9 +229,8 @@ glnx_fd_get_all_xattrs (int            fd,
                         GCancellable  *cancellable,
                         GError       **error)
 {
-  char buf[PATH_MAX];
-  snprintf (buf, sizeof (buf), "/proc/self/fd/%d", fd);
-  return get_xattrs_impl (buf, -1, out_xattrs, cancellable, error);
+  return get_xattrs_impl (NULL, fd, out_xattrs,
+                          cancellable, error);
 }
 
 /**
