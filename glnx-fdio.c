flatpak/flatpak

commit 1345882d6a5fc3ea851bfe5510e79861d511c25e

glnx_file_copy_at: Add GLNX_FILE_COPY_NOCHOWN
In some contexts, we may want to copy a root-owned file but we're not
running as root so we can't `fchown` it. (The case I'm interested in is
actually a bit more obscure than this: running in a supermin VM as root,
and wanting to copy a file we created onto a 9p mount where we don't
have perms to `fchown`).

Add a `GLNX_FILE_COPY_NOCHOWN` to handle this case.

glnx-fdio.c
@@ -1000,8 +1000,11 @@ glnx_file_copy_at (int                   src_dfd,
   if (glnx_regfile_copy_bytes (src_fd, tmp_dest.fd, (off_t) -1) < 0)
     return glnx_throw_errno_prefix (error, "regfile copy");
 
-  if (fchown (tmp_dest.fd, src_stbuf->st_uid, src_stbuf->st_gid) != 0)
-    return glnx_throw_errno_prefix (error, "fchown");
+  if (!(copyflags & GLNX_FILE_COPY_NOCHOWN))
+    {
+      if (fchown (tmp_dest.fd, src_stbuf->st_uid, src_stbuf->st_gid) != 0)
+        return glnx_throw_errno_prefix (error, "fchown");
+    }
 
   if (!(copyflags & GLNX_FILE_COPY_NOXATTRS))
     {

glnx-fdio.h
@@ -189,7 +189,8 @@ glnx_regfile_copy_bytes (int fdf, int fdt, off_t max_bytes);
 typedef enum {
   GLNX_FILE_COPY_OVERWRITE = (1 << 0),
   GLNX_FILE_COPY_NOXATTRS = (1 << 1),
-  GLNX_FILE_COPY_DATASYNC = (1 << 2)
+  GLNX_FILE_COPY_DATASYNC = (1 << 2),
+  GLNX_FILE_COPY_NOCHOWN = (1 << 3)
 } GLnxFileCopyFlags;
 
 gboolean

commit ff7802406001c367e94f05ee4abe6f172fbabd62

fdio: Don't change ownership of symlinks if not requested
Fixes: 1345882d "glnx_file_copy_at: Add GLNX_FILE_COPY_NOCHOWN"
Resolves: https://gitlab.gnome.org/GNOME/libglnx/-/work_items/8
Resolves: https://github.com/flatpak/flatpak-builder/issues/770
Signed-off-by: Simon McVittie <smcv@collabora.com>

glnx-fdio.c
@@ -745,9 +745,12 @@ copy_symlink_at (int                   src_dfd,
         return FALSE;
     }
 
-  if (TEMP_FAILURE_RETRY (fchownat (fd, "", src_stbuf->st_uid, src_stbuf->st_gid,
-                                    AT_EMPTY_PATH)) != 0)
-    return glnx_throw_errno_prefix (error, "fchownat");
+  if (!(copyflags & GLNX_FILE_COPY_NOCHOWN))
+    {
+      if (TEMP_FAILURE_RETRY (fchownat (fd, "", src_stbuf->st_uid, src_stbuf->st_gid,
+                                        AT_EMPTY_PATH)) != 0)
+        return glnx_throw_errno_prefix (error, "fchownat");
+    }
 
   return TRUE;
 }